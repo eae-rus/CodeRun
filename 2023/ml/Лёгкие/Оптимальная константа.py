caaa8586decee7fe9bf38f715d5aa3423d0cf7a0
eae_rus <eae_rus@mail.ru>
2023-04-29 21:36:59 +0300

    Совсем удалена сортировка
    Тест всё равно провален по времени.

--- 2023/ml/Лёгкие/Оптимальная константа.py
+++ 2023/ml/Лёгкие/Оптимальная константа.py
@@ -6,7 +6,7 @@ def main():
     MSE можно найти за счёт математических преобразований (производной) как:
     - среднее арифметическое входных чисел делённое на число элементов
     MAE и MAPE будем искать за счёт:
-    - минимального отклонения MAE и MAPE по массиву уникальных числе начиная с меньшего значения
+    - минимального отклонения MAE и MAPE по массиву уникальных чисел
     *Примечание: последнее утверждение является не проверенной гипотизой
     '''
     sample_size = int(input())
@@ -20,28 +20,24 @@ def main():
     print(answer_mse)
 
     # дальше создаём сортированный массив уникальных значений
-    sorted_set_elements = __quicksort__(list(set(selection_elements)))
+    set_elements = list(set(selection_elements))
     # используем его для поиска минимального абсолютного отклонения
-    mae = calculate_mae(selection_elements, sorted_set_elements[0])
-    answer_mae = sorted_set_elements[0]
-    for elemet in sorted_set_elements[1:]:
+    mae = calculate_mae(selection_elements, set_elements[0])
+    answer_mae = set_elements[0]
+    for elemet in set_elements[1:]:
         new_mae = calculate_mae(selection_elements, elemet)
         if new_mae < mae:
             mae = new_mae
             answer_mae = elemet
-        else:
-            break
     print(answer_mae)
     # используем его для поиска минимального абсолютного отклонения
-    mape = calculate_mape(selection_elements, sorted_set_elements[0])
-    answer_mape = sorted_set_elements[0]
-    for elemet in sorted_set_elements[1:]:
+    mape = calculate_mape(selection_elements, set_elements[0])
+    answer_mape = set_elements[0]
+    for elemet in set_elements[1:]:
         new_mape = calculate_mape(selection_elements, elemet)
         if new_mape < mape:
             mape = new_mape
             answer_mape = elemet
-        else:
-            break
     print(answer_mape)
 
 def calculate_mae(list1: list, shift: float) -> float:
@@ -74,14 +70,6 @@ def __relative_difference_list__(list1: list, list2: list) -> list:
         relative_difference[i] = (list1[i] - list2[i])/list1[i]
     return relative_difference
 
-def __quicksort__(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        less = [x for x in arr[1:] if x <= pivot]
-        greater = [x for x in arr[1:] if x > pivot]
-        return __quicksort__(less) + [pivot] + __quicksort__(greater)
 
 if __name__ == '__main__':
 	main()
