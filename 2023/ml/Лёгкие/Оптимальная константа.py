5596ab5f8f09bd7d50db3a4e81e2b7f8a2a6d5f2
eae_rus <eae_rus@mail.ru>
2023-05-01 17:24:32 +0300

    Измена оптимальная константа
    Упрощён / сокращён алгоритм расчёта, убраны лишние блоки.
    
    По времени всё равно не проходит, хотя ответы стали ближе к требуемым

--- 2023/ml/Лёгкие/Оптимальная константа.py
+++ 2023/ml/Лёгкие/Оптимальная константа.py
@@ -15,8 +15,7 @@ def main():
     selection_elements = [None] * sample_size
         
     for i in range(sample_size):
-        sample = int(input())
-        selection_elements[i]=sample
+        selection_elements[i] = int(input())
     
     answer_mse = sum(selection_elements)/sample_size
     print(answer_mse)
@@ -33,33 +32,17 @@ def main():
 
 def calculate_mae(elements: list, shift: float) -> float:
     # FIXME: отутствует какая-либо "защита" от невалидных значений
-    difference = __difference_list__(elements, shift)
-    abs_difference = [abs(x) for x in difference]
-    mae = sum(abs_difference)
-    return mae
+    sum = 0
+    for i in range(len(elements)):
+        sum += abs(elements[i] - shift)
+    return sum
 
 def calculate_mape(elements: list, shift: float) -> float:
     # FIXME: отутствует какая-либо "защита" от невалидных значений
-    relative_difference = __relative_difference_list__(elements, shift)
-    abs_difference = [abs(x) for x in relative_difference]
-    mape = sum(abs_difference)
-    return mape
-
-def __difference_list__(list1: list, min_elements: int) -> list: 
-    # FIXME: отутствует какая-либо "защита" от невалидных значений
-    difference = [None]*len(list1)
-    # Используем цикл for для вычисления разности между соответствующими элементами двух списков
-    for i in range(len(list1)):
-        difference[i] = list1[i] - min_elements
-    return difference
-
-def __relative_difference_list__(list1: list, min_elements: int) -> list: 
-    # FIXME: отутствует какая-либо "защита" от невалидных значений
-    relative_difference = [None]*len(list1)
-    # Используем цикл for для вычисления разности между соответствующими элементами двух списков
-    for i in range(len(list1)):
-        relative_difference[i] = (list1[i] - min_elements)/list1[i]
-    return relative_difference
+    sum = 0
+    for i in range(len(elements)):
+        sum += abs((elements[i] - shift)/elements[i])
+    return sum
 
 def find_opimal_constant_mae_grad(selection_elements: list, max_value: int, min_value: int) -> float:
     # FIXME: отутствует какая-либо "защита" от невалидных значений
@@ -67,26 +50,14 @@ def find_opimal_constant_mae_grad(selection_elements: list, max_value: int, min_
     if min_value == max_value:
         return min_value
     
-    derivative = (calculate_mae(selection_elements, min_value + 1e-7)- 
-                  calculate_mae(selection_elements, min_value))
-    if derivative >= 0:
-        return min_value
-    
-    derivative = (calculate_mae(selection_elements, max_value) - 
-                  calculate_mae(selection_elements, max_value - 1e-7))
-    if derivative <= 0:
-        return max_value
-    
     opimal_constant = (max_value + min_value) / 2
     while abs(max_value - min_value) > 1e-6:
         derivative = (calculate_mae(selection_elements, opimal_constant) - 
                       calculate_mae(selection_elements, opimal_constant - 1e-7))
         if derivative > 0:
             max_value = opimal_constant
-        elif derivative < 0:
-            min_value = opimal_constant
         else:
-            return opimal_constant
+            min_value = opimal_constant
         
         opimal_constant = (max_value + min_value) / 2
              
@@ -98,26 +69,14 @@ def find_opimal_constant_mape_grad(selection_elements: list, max_value: int, min
     if min_value == max_value:
         return min_value
     
-    derivative = (calculate_mape(selection_elements, min_value + 1e-7)- 
-                  calculate_mape(selection_elements, min_value))
-    if derivative >= 0:
-        return min_value
-    
-    derivative = (calculate_mape(selection_elements, max_value) - 
-                  calculate_mape(selection_elements, max_value - 1e-7))
-    if derivative < 0:
-        return max_value
-    
     opimal_constant = (max_value + min_value) / 2
     while abs(max_value - min_value) > 1e-6:
         derivative = (calculate_mape(selection_elements, opimal_constant) - 
                       calculate_mape(selection_elements, opimal_constant - 1e-7))
         if derivative > 0:
             max_value = opimal_constant
-        elif derivative < 0:
-            min_value = opimal_constant
         else:
-            return opimal_constant
+            min_value = opimal_constant
         
         opimal_constant = (max_value + min_value) / 2
              
