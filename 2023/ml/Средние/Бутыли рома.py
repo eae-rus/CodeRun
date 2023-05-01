03490fa56a591ae1564526c5bad37406e4040a2a
eae_rus <eae_rus@mail.ru>
2023-05-01 22:58:29 +0300

    Измена бутылка рома
    Нашёл нужную формулу... НО она дейтсвует только на весь массив, а как для подмасивов и как их перебирать? Вот это не знаю и пока не решил
    
    Тест по памяти всё равно провален, И теперь даже 70, где-то я потратил лишнюю память, сейчас спрошу у GPT

--- 2023/ml/Средние/Бутыли рома.py
+++ 2023/ml/Средние/Бутыли рома.py
@@ -1,4 +1,6 @@
+import math
 from itertools import *
+from collections import Counter
 
 def main():
     '''
@@ -6,15 +8,29 @@ def main():
     text = input()
     
     sum_combinations = 0
-    for z in range(1,len(text)+1):
+    for z in range(1,len(text)):
         gen_combinations = (''.join(comb) for comb in permutations(text, z))
         set_combinations = set()
         for comb in gen_combinations:
             if comb not in set_combinations:
                 set_combinations.add(comb)
                 sum_combinations += 1
+    text_list = [char for char in text]
+    dict_text = count_occurrences(set(text_list), text_list)
+    result = math.factorial(len(text))
+    for key, value in dict_text.items():
+        result /= math.factorial(value)
+    sum_combinations+=result
         
-    print(sum_combinations)
+    print(int(sum_combinations))
+
+
+def count_occurrences(elements, lst):
+    counts = Counter(lst)
+    result = {}
+    for e in elements:
+        result[e] = counts.get(e, 0)
+    return result
 
 if __name__ == '__main__':
 	main()
