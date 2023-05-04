71bfde3f0506658d9242e3515f880d68524fd3ec
eae_rus <eae_rus@mail.ru>
2023-05-04 21:44:58 +0300

    Изменен решающий пень
    Переписал функцию на один параметр (с)
    
    Проблему не решает. Судя по их значенями, они делали что-то типо "вычисляем середину", но у меня почему-то не работает, да и не верится, если честно, что это будет работать.

--- 2023/ml/Лёгкие/Решающий пень.py
+++ 2023/ml/Лёгкие/Решающий пень.py
@@ -7,33 +7,30 @@ def main():
     for i in range(sample_size):
         x[i], y[i] = map(int, input().split())
 
-    coeffs = find_coeffs(x, y)
+    coeffs = find_coeffs(x, y, sample_size)
     print(" ".join(map(str, coeffs)))
 
-def f(x, y_low, y_uper, a, b):
-    return np.square(y_low - a).sum() + np.square(y_uper - b).sum()
 
-def find_coeffs(x, y):
+def f(x, y, c, sample_size):
+    y_low = y[x < c]
+    y_uper = y[x >= c]
+    len_y_low = len(y_low)
+    len_y_upper = sample_size - len_y_low
+    a = np.mean(y_low) if len_y_low != 0 else 0
+    b = np.mean(y_uper) if len_y_upper != 0 else 0
+    f_value = np.square(y_low - a).sum() + np.square(y_uper - b).sum()
+    return [a, b, f_value]
+
+def find_coeffs(x, y, sample_size):
     set_x = np.unique(x)
-    c_opt = x[0]
-    y_low = y[x < c_opt]
-    y_uper = y[x >= c_opt]
-    a_opt = np.mean(y_low) if len(y_low) != 0 else 0
-    b_opt = np.mean(y_uper) if len(y_uper) != 0 else 0
-    f_min = f(x, y_low, y_uper, a_opt, b_opt)
+    c_opt = set_x[0]
+    a_opt, b_opt, f_min = f(x, y, c_opt, sample_size)
 
     for c_i in set_x:
-        y_low = y[x < c_i]
-        y_uper = y[x >= c_i]
-        a_i = np.mean(y_low) if len(y_low) != 0 else 0
-        b_i = np.mean(y_uper) if len(y_uper) != 0 else 0
-        f_i = f(x, y_low, y_uper, a_i, b_i)
-
+        a_i, b_i, f_i = f(x, y, c_i, sample_size)
         if f_i < f_min:
             a_opt, b_opt, c_opt, f_min = a_i, b_i, c_i, f_i
-
     return [a_opt, b_opt, c_opt]
-
 
 if __name__ == '__main__':
 	main()
