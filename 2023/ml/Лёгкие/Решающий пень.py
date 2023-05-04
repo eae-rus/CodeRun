68e6009ad70dd7dd5b8ca89558e13acc9389db08
eae_rus <eae_rus@mail.ru>
2023-05-04 18:33:11 +0300

    Изменен решающий пень
    Оптимизировал вариант с перебором. Но надо ещё думать как его ускорить.
    
    Предыдущий спуск к лучшему, увы, не помог и местами давал неверный результат, вероятно, он не всегда сходится.

--- 2023/ml/Лёгкие/Решающий пень.py
+++ 2023/ml/Лёгкие/Решающий пень.py
@@ -1,67 +1,39 @@
+
 import numpy as np
 
 def main():
     
     sample_size = int(input())
-    x = np.empty(sample_size)
-    y = np.empty(sample_size)
-        
+    x, y = np.empty(sample_size), np.empty(sample_size)
     for i in range(sample_size):
-        sample = list(map(int, input().split()))
-        x[i] = sample[0]
-        y[i] = sample[1]
+        x[i], y[i] = map(int, input().split())
 
-    coeffs = find_coeffs(x, y, sample_size)
+    coeffs = find_coeffs(x, y)
     print(" ".join(map(str, coeffs)))
 
-def f(x, y, a, b, c, sample_size):
+def f(x, y, a, b, c):
     y_low = y[x < c]
     y_uper = y[x >= c]
     sum = np.sum((y_low - a)**2) + np.sum((y_uper - b)**2)
-    return (sum/sample_size)**0.5 
+    return sum
 
-def find_coeffs(x, y, sample_size):
-    sort_set_x = np.sort(np.unique(x))
-    len_sort_set_x = len(sort_set_x)
-    a_opt = y[0]
-    b_opt = y[0]
-    c_opt = x[0]
-    
-    low, upper = 0, len_sort_set_x
-    while (upper - low > 0):
-        mid_up = int((low + upper) // 2)
-        c_up = sort_set_x[mid_up]
-        a_up, b_up = find_ab(x, y, c_up, sample_size)
-        f_i_up = f(x, y, a_up, b_up, c_up, sample_size)
-        
-        mid_low = mid_up-1
-        c_low = sort_set_x[mid_low]
-        a_low, b_low = find_ab(x, y, c_low, sample_size)
-        f_i_low = f(x, y, a_low, b_low, c_low, sample_size)
-        
-        if f_i_up - f_i_low > 0:
-            upper = mid_low
-            a_opt = a_low
-            b_opt = b_low
-            c_opt = c_low
-        else:
-            low = mid_up
-            a_opt = a_up
-            b_opt = b_up
-            c_opt = c_up
+def find_coeffs(x, y):
+    set_x = np.unique(x)
+    a_opt, b_opt, c_opt = y[0], y[0], x[0]
+    f_min = f(x, y, a_opt, b_opt, c_opt)
+
+    for c_i in set_x:
+        y_low = y[x < c_i]
+        y_uper = y[x >= c_i]
+        a_i = np.mean(y_low) if len(y_low) != 0 else 0
+        b_i = np.mean(y_uper) if len(y_uper) != 0 else 0
+        f_i = f(x, y, a_i, b_i, c_i)
+
+        if f_i < f_min:
+            a_opt, b_opt, c_opt, f_min = a_i, b_i, c_i, f_i
 
     return [a_opt, b_opt, c_opt]
 
-def find_ab(x, y, c, sample_size):
-    y_low = y[x < c]
-    y_uper = y[x >= c]
-    sum_low = np.sum(y_low)
-    sum_uper = np.sum(y_uper)
-    len_low = len(y_low)
-    len_uper = len(y_uper)
-    a = sum_low / len_low if len_low != 0 else 0
-    b = sum_uper / len_uper if len_uper != 0 else 0
-    return a, b
 
 if __name__ == '__main__':
 	main()
