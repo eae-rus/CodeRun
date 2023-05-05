af700d600f057423b1d67cac10f50bfbe72bc48b
eae_rus <eae_rus@mail.ru>
2023-05-05 23:26:50 +0300

    Измененена "Исчезнувшая"
    Добавил скользящую функцию.
    Теперь она заработала, отлично))
    
    *Смотрел решение ещё одного участника, он делелал через хеши. Себе на будущее - я неправильно их использую, надо было как я сумму, так же хеши проверять, это на порядок правильнее, а суть похожа

--- 2023/ml/Средние/Исчезнувшая/Исчезнувшая.py
+++ 2023/ml/Средние/Исчезнувшая/Исчезнувшая.py
@@ -33,14 +33,15 @@ def main() -> None:
 def find_foto_in_all_planes(photo_1: np.ndarray, photo_2: np.ndarray) -> bool:
     '''
     '''
+    sum_foto_1 = np.sum(photo_1)
     for _ in range(4):
-        if find_foto(photo_1, photo_2):
+        if find_foto(photo_1, photo_2, sum_foto_1):
             return True
         else:
             photo_2 = np.transpose(photo_2[::-1,:])
     return False
 
-def find_foto(photo_1: np.ndarray, photo_2: np.ndarray) -> bool:
+def find_foto(photo_1: np.ndarray, photo_2: np.ndarray, sum_foto_1: int) -> bool:
     '''
     '''
     n1, m1 = photo_1.shape
@@ -56,10 +57,20 @@ def find_foto(photo_1: np.ndarray, photo_2: np.ndarray) -> bool:
             return False
 
     else:
+        sum_foto_2_start = np.sum(photo_2[0:n1,0:m1])
         for i in range(n2 - n1+1):
+            if i>0:
+                sum_foto_2_start -= np.sum(photo_2[i-1:i,0:m1])
+                sum_foto_2_start += np.sum(photo_2[i+n1-1:i+n1,0:m1])
+            
+            sum_foto_2 = sum_foto_2_start
             for k in range(m2 - m1+1):
-                if np.array_equal(photo_1, photo_2[i:i+n1, k:k+m1]):
-                    return True
+                if k>0:
+                    sum_foto_2 -= np.sum(photo_2[i:i+n1,k-1:k])
+                    sum_foto_2 += np.sum(photo_2[i:i+n1,k+m1-1:k+m1])
+                if sum_foto_2 == sum_foto_1:
+                    if np.array_equal(photo_1, photo_2[i:i+n1, k:k+m1]):
+                        return True
         return False
 
 @lru_cache(maxsize=None)
