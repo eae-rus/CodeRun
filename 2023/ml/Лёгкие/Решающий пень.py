7e308db2686f34be16210d51989970046cffa164
eae_rus <eae_rus@mail.ru>
2023-05-12 23:23:56 +0300

    Изменен решающий пень
    Перенёс функции внутрь

--- 2023/ml/Лёгкие/Решающий пень.py
+++ 2023/ml/Лёгкие/Решающий пень.py
@@ -1,32 +1,32 @@
 import numpy as np
 
-class MeanCalculator:
-    def __init__(self):
-        self.Count = 0.
-        self.Mean = 0.
-
-    def Add(self, value, weight = 1.):
-        self.Count += weight
-        self.Mean += weight * (value - self.Mean) / self.Count
-
-    def Remove(self, value, weight = 1.):
-        self.Add(value, -weight)
-
-class SumSquaredErrorsCalculator:
-    def __init__(self):
-        self.MeanCalculator = MeanCalculator()
-        self.SSE = 0.
-
-    def Add(self, value, weight = 1.):
-        curDiff = value - self.MeanCalculator.Mean
-        self.MeanCalculator.Add(value, weight)
-        self.SSE += weight * curDiff * (value - self.MeanCalculator.Mean)
-
-    def Remove(self, value, weight = 1.):
-        self.Add(value, -weight)
-
-
 def main():
+    
+    class MeanCalculator:
+        def __init__(self):
+            self.Count = 0.
+            self.Mean = 0.
+    
+        def Add(self, value, weight = 1.):
+            self.Count += weight
+            self.Mean += weight * (value - self.Mean) / self.Count
+    
+        def Remove(self, value, weight = 1.):
+            self.Add(value, -weight)
+    
+    class SumSquaredErrorsCalculator:
+        def __init__(self):
+            self.MeanCalculator = MeanCalculator()
+            self.SSE = 0.
+    
+        def Add(self, value, weight = 1.):
+            curDiff = value - self.MeanCalculator.Mean
+            self.MeanCalculator.Add(value, weight)
+            self.SSE += weight * curDiff * (value - self.MeanCalculator.Mean)
+    
+        def Remove(self, value, weight = 1.):
+            self.Add(value, -weight)
+    
     OverAllSSE = SumSquaredErrorsCalculator()
     
     sample_size = int(input())
