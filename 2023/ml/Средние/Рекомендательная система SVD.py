c0a913b6fa34ec5653ac0f8b295d2721102dc3b0
eae_rus <eae_rus@mail.ru>
2023-05-19 20:22:10 +0300

    Изменена " Рекомендательная система SVD"
    Переписана на "свой код", хотя, это сгенерила ChatGPT, но я ряд моментов допилил, вроде бы должно работать, сейчас проверим

--- 2023/ml/Средние/Рекомендательная система SVD.py
+++ 2023/ml/Средние/Рекомендательная система SVD.py
@@ -1,78 +1,69 @@
 import numpy as np
-from scipy.sparse.linalg import svds
+
+
+class SVD:
+    def __init__(self, n_factors=10, n_epochs=20, lr=0.005, reg=0.02, k_max=10):
+        self.n_factors = n_factors  # число скрытых факторов
+        self.n_epochs = n_epochs  # число эпох обучения
+        self.lr = lr  # скорость обучения
+        self.reg = reg  # коэффициент регуляризации
+        self.k_max = k_max  # максимальная оценка
+
+    def fit(self, X, n_users, n_items):
+        self.mean = np.mean(X[:, 2]) / self.k_max  # среднее значение оценок
+        self.user_ids, self.item_ids = np.arange(n_users), np.arange(n_items)
+        self.n_users, self.n_items = n_users, n_items
+
+        # инициализируем веса
+        self.b_u = np.zeros(self.n_users)
+        self.b_i = np.zeros(self.n_items)
+        self.q_i = np.random.normal(scale=1. / self.n_factors, size=(self.n_items, self.n_factors))
+        self.p_u = np.random.normal(scale=1. / self.n_factors, size=(self.n_users, self.n_factors))
+
+        # обучение
+        for epoch in range(self.n_epochs):
+            for u, i, r in X:
+                # обновляем веса
+                dot = np.dot(self.q_i[i], self.p_u[u])
+                err = r / self.k_max - (self.mean + self.b_u[u] + self.b_i[i] + dot)
+                self.b_u[u] += self.lr * (err - self.reg * self.b_u[u])
+                self.b_i[i] += self.lr * (err - self.reg * self.b_i[i])
+                self.p_u[u, :] += self.lr * (err * self.q_i[i] - self.reg * self.p_u[u, :])
+                self.q_i[i, :] += self.lr * (err * self.p_u[u] - self.reg * self.q_i[i, :])
+                
+    def predict(self, u, i):
+        dot = np.dot(self.q_i[i], self.p_u[u])
+        pred_rating = self.k_max * (self.mean + self.b_u[u] + self.b_i[i] + dot)
+        pred_rating = pred_rating if pred_rating <= self.k_max else self.k_max
+        pred_rating = pred_rating if pred_rating >= 0 else 0
+        return pred_rating
 
 def main():
-    def predict_rating(user_id, movie_id):
-        # получаем вектор пользователя
-        
-        user_vector = np.multiply(U[user_id, :], sigma.T)
-
-        # получаем вектор фильма
-        movie_vector = V[:, movie_id]
-
-        # вычисляем предсказанную оценку
-        predicted_rating = user_vector.dot(movie_vector)
-        predicted_rating += mean_rating
-        predicted_rating += user_bias[user_id] + movie_bias[movie_id]
-
-        if predicted_rating > 1:
-            return k_value
-        elif predicted_rating < 0:
-            return 0
-        else:
-            return predicted_rating * k_value
-
     # чтение входных данных
     k_value, U_value, M_value, D_value, T_value = map(int, input().split())
-    U_M_train = np.full((U_value, M_value), np.nan, dtype=np.float32)
-    all_ratings = 0
-    all_ratings_count = 0
-    user_bias = np.zeros(U_value)
-    user_bias_count = np.zeros(U_value, dtype=np.int32)
-    movie_bias = np.zeros(M_value)
-    moveie_bias_count = np.zeros(M_value, dtype=np.int32)
-    for _ in range(D_value):
+    # TODO: Подумать о реализации разреженной матрицы
+    U_M_train = np.full((D_value, 3), np.nan, dtype=np.int32)
+    for i in range(D_value):
         user, movie, rating = map(int, input().split())
-        rating /= k_value
+        rating
         # занесение данных в матрицу
-        U_M_train[user, movie] = rating
-        # вычисления средних
-        all_ratings += rating
-        all_ratings_count += 1
-        user_bias[user] += rating
-        user_bias_count[user] += 1
-        movie_bias[movie] += rating
-        moveie_bias_count[movie] += 1
-
-    # вычисляем среднее значение оценок
-    mean_rating = all_ratings / all_ratings_count
-
-    # получаем смещения по пользователю и фильму
-    user_bias = user_bias / user_bias_count - mean_rating
-    movie_bias = movie_bias / moveie_bias_count - mean_rating
-
-    # обработка пропущенных значений
-    mean_ratings = np.nanmean(U_M_train, axis=1)
-    for i in range(U_value):
-        if user_bias_count[i] >= 1:
-            for k in range(M_value):
-                if np.isnan(U_M_train[i][k]):
-                    U_M_train[i][k] = mean_ratings[i]
-        else:
-            for k in range(M_value):
-                if np.isnan(U_M_train[i][k]):
-                    U_M_train[i][k] = mean_rating
-
-    # выполняем SVD разложение
+        U_M_train[i, 0] = user
+        U_M_train[i, 1] = movie
+        U_M_train[i, 2] = rating
+    
     min_dim = 10
     if U_value <= 10 or M_value <= 10:
         min_dim = min(U_value, M_value) - 1
-    U, sigma, V = svds(U_M_train - mean_rating, k=min_dim)
+    
+    svd = SVD(n_factors=min_dim, lr=0.05, reg=0.1, n_epochs=50, k_max=k_value)
+    
+    svd.fit(U_M_train, U_value, M_value)
 
+    
     # вычисление предсказаний и вывод результатов
     for _ in range(T_value):
         user, movie = map(int, input().split())
-        rating = predict_rating(user, movie)
+        rating = svd.predict(user, movie)
         print(rating)
 
 
