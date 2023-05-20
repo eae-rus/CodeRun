e5a126224f615557fbd423d1d880e946134f5224
eae_rus <eae_rus@mail.ru>
2023-05-20 12:22:26 +0300

    Изменена "Коллекция статуэток"
    Немного сокрректирован порядок расчёта, ChatGPT говорит, что это должно ускорить процесс, как-то не очень верится... Да, завалился на том же тесте.

--- 2023/ml/Средние/Коллекция статуэток.py
+++ 2023/ml/Средние/Коллекция статуэток.py
@@ -1,28 +1,26 @@
+from collections import defaultdict
+
 def main():
     """
     """
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     
-    counts = {}
+    counts = defaultdict(int)
     l, r = 0, 0
     cost = float('inf')
-    while l < n:
-        while len(counts) < k and r < n:
-            if a[r] <= k:
-                if a[r] not in counts:
-                    counts[a[r]] = 1
-                else:
-                    counts[a[r]] += 1
-            r += 1
-        if len(counts) == k:
-            cost = min(cost, sum(a[l:r]))
+    while r < n:
+        if a[r] <= k:
+            counts[a[r]] += 1
+        r += 1
         
-        if a[l] <= k:
-            counts[a[l]] -= 1
-            if counts[a[l]] == 0:
-                del counts[a[l]]
-        l += 1
+        while len(counts) == k:
+            cost = min(cost, sum(a[l:r]))
+            if a[l] <= k:
+                counts[a[l]] -= 1
+                if counts[a[l]] == 0:
+                    del counts[a[l]]
+            l += 1
     
     print(cost)
 
