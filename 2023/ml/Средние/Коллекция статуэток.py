12f8e0d0293f4f5b9c56ea904aa62813356dcd9c
eae_rus <eae_rus@mail.ru>
2023-05-20 12:03:23 +0300

    Изменена "Коллекция статуэток"
    Проверка была переведена в булевую плоскость + добавлен счётчик
    
    Превышено время расчёта.

--- 2023/ml/Средние/Коллекция статуэток.py
+++ 2023/ml/Средние/Коллекция статуэток.py
@@ -6,20 +6,24 @@ def main():
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     
-    counts = np.zeros(k)
+    signal_avail = np.full(k, False, dtype=np.bool_)
+    counts_cont = np.zeros(k, dtype=np.int32)
     l, r = 0, 0
     cost = float('inf')
     while l < n:
-        while counts.sum() < k and r < n:
+        while (not signal_avail.all()) and r < n:
             if a[r] <= k:
-                if counts[a[r]-1] == 0:
-                    counts[a[r]-1] += 1
+                counts_cont[a[r]-1] += 1
+                if (not signal_avail[a[r]-1]):
+                    signal_avail[a[r]-1] = True
             r += 1
-        if counts.sum() == k:
+        if signal_avail.all():
             cost = min(cost, sum(a[l:r]))
         
-        if a[l] <= k:    
-            counts[a[l]-1] -= 1
+        if a[l] <= k:
+            counts_cont[a[l]-1] -= 1
+            if counts_cont[a[l]-1] == 0:
+                signal_avail[a[l]-1] = False
         l += 1
     
     print(cost)
