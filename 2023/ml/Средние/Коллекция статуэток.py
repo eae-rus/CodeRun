a5a846ff1c1efe07fbb61c750abc0242ced1caa3
eae_rus <eae_rus@mail.ru>
2023-05-20 12:39:03 +0300

    Изменена "Коллекция статуэток"
    Добавил скользящую сумму

--- 2023/ml/Средние/Коллекция статуэток.py
+++ 2023/ml/Средние/Коллекция статуэток.py
@@ -8,18 +8,21 @@ def main():
     
     counts = defaultdict(int)
     l, r = 0, 0
+    sum_a = 0
     cost = float('inf')
     while r < n:
         if a[r] <= k:
             counts[a[r]] += 1
+        sum_a += a[r]
         r += 1
         
         while len(counts) == k:
-            cost = min(cost, sum(a[l:r]))
+            cost = min(cost, sum_a)
             if a[l] <= k:
                 counts[a[l]] -= 1
                 if counts[a[l]] == 0:
                     del counts[a[l]]
+            sum_a -= a[l]
             l += 1
     
     print(cost)
