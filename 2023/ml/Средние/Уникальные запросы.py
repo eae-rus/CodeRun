ca0567152831a0bf188712c8aaccee40cdc81511
eae_rus <eae_rus@mail.ru>
2023-05-21 17:39:39 +0300

    Изменена "Уникальные запросы"
    Перевёл в расчёты в 2-ичной системе исчисления с приведением к близком числу. Увы, не дало нужно результата (вероятно, некорректное приведение для другой системы исчисления)

--- 2023/ml/Средние/Уникальные запросы.py
+++ 2023/ml/Средние/Уникальные запросы.py
@@ -4,12 +4,15 @@ def main():
     """_summary_
     """
     n = int(input())
-    m = 199900
+    m = 131072 # 2**17
     bits = [False] * m
     unique_count = 0
+    degree_divisor = 140737488355328 # 2**47
+    degree_add = 65536 # 2**16
 
     for _ in range(n):
-        h = hash(sys.stdin.readline()) % m
+        h = hash(sys.stdin.readline())
+        h = int(h / degree_divisor) + degree_add
         if not bits[h]:
             unique_count += 1
             bits[h] = True
