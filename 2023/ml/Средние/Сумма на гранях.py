19ab5907f4a1d9aedf05dea39ecb64a0d9e1d317
eae_rus <eae_rus@mail.ru>
2023-05-21 13:42:43 +0300

    Изменена "сумма на гранях"
    Перед сном пришла идея, как правильно считать ожидаемую сумму на следующем броске, и да, она оказалась верной. Сон творит чудеса!

--- 2023/ml/Средние/Сумма на гранях.py
+++ 2023/ml/Средние/Сумма на гранях.py
@@ -1,30 +1,24 @@
-import sys
-
-
 def main():
     '''
-    Реализованно за счёт рекурентной функции
+    Реализованно за счёт предполагаемой формулы
     '''
     cube_faces = list(map(int, input().split()))
     number_throws = int(input())
-
-    cube_faces_dict = {face: cube_faces.count(face)/6 for face in set(cube_faces)}
     
-    mathematical_expectation = sum_faces_tree(cube_faces_dict, number_throws)
+    b1_sum = sum(cube_faces)/6
     
-    print(mathematical_expectation)
-
-def sum_faces_tree(cube_faces_dict, throws_left: int, before_sum: float = 0, before_probability: float = 1, before_face: int = -1):
-    if throws_left > 0:
-        sum = 0
-        for face, probability in cube_faces_dict.items():
-            if face != before_face:
-                sum += sum_faces_tree(cube_faces_dict, throws_left - 1, before_sum+face, before_probability*probability, face)
-            else:
-                sum += sum_faces_tree(cube_faces_dict, throws_left - 1, before_sum, before_probability*probability, face)
-        return sum
-    else:
-        return before_sum * before_probability
+    bx = 0
+    
+    for x_previous in cube_faces:
+        for x in cube_faces:
+            if x != x_previous:
+                bx += x
+    
+    bx_sum = bx/36
+    
+    sum_k = b1_sum + (number_throws - 1) * bx_sum
+    
+    print(sum_k)
 
 if __name__ == '__main__':
 	main()
