0899565e5a9c9feb221f38b05558d08a065d8021
eae_rus <eae_rus@mail.ru>
2023-05-22 23:47:29 +0300

    + Добавлено начало "D-CoV-3999"
    Добавил задачу, решил пока совсем в лоб, иии, вероятно, только тестовую задачу. Сейчас будем смотреть на тестах, почти уверен, что где-то по скорости или памяти не пройду, и надо как-то иначе её решать.
    
    Странно, ошибка во время выполнения, будем искать косяки.

---
+++ 2023/ml/Средние/D-CoV-3999.py
@@ -0,0 +1,40 @@
+import sys
+import numpy as np
+
+def main():
+    '''
+    '''
+                
+    # Чтение входных данных
+    n = int(input())
+    status = np.full(n, False, dtype=np.uint8)
+    dict_visited = {}
+    # Чтение заражённых сотрудников
+    status = np.array(list(map(int, input().split()))).astype(np.uint8)
+    
+    for i in range(0,n):
+        k, *meetings = map(int, input().split())
+        for j in meetings:
+            if dict_visited.get(k) is None:
+                dict_visited[j] = [i, -1]
+            else:
+                dict_visited[j][1] = i
+    
+    visited = np.array([(k, *v) for k, v in dict_visited.items()])
+    # Получение индексов, отсортированных по первому столбцу
+    sorted_indices = np.argsort(visited[:, 0])
+    
+    for i in sorted_indices:
+        employee_1 = visited[i][1]
+        employee_2 = visited[i][2]
+        if employee_1 != -1 and employee_2 != -1:
+            if status[employee_1] or status[employee_2]:
+                status[employee_1] = 1
+                status[employee_2] = 1   
+
+    # Вывод статусов сотрудников
+    print(' '.join(map(str, status)))
+    
+
+if __name__ == '__main__':
+	main()
