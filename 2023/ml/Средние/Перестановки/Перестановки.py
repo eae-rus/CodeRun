8f12945da62dc52c4aa713ab6685a55c87b3d465
eae_rus <eae_rus@mail.ru>
2023-05-22 11:46:08 +0300

    Изменена "Перестановка"
    Черновой вариант с проработкой гипотез.

--- 2023/ml/Средние/Перестановки/Перестановки.py
+++ 2023/ml/Средние/Перестановки/Перестановки.py
@@ -5,70 +5,51 @@ import numpy as np
 def main():
     '''
     '''
+    #file_path_example = os.path.abspath("") + '\\2023\\ml\\Средние\\Перестановки\\example1.txt'
+    #WriteExample(file_path_example)
+    
     file_path = os.path.abspath("") + '\\2023\\ml\\Средние\\Перестановки\\permutations.in'
+    #file_path = os.path.abspath("") + '\\2023\\ml\\Средние\\Перестановки\\example.txt'
     with open(file_path, 'r') as f:
         s = f.readline()[:-1]
         n = int(s)
-
         good_sets = []
         stupid_sets = []
-
-        for i in range(0, n, 2):
-            numeric_matrix_1 = np.zeros((8, 8), dtype=np.uint16)
+        for i in range(0, n):
+            numeric_matrix_1 = np.zeros((8, 8), dtype=np.uint32)
             for j in range(1000):
                 perm = list(map(int, f.readline()[:-1].split()))
                 for k in range(8):
                     numeric_matrix_1[k][perm[k]-1] += 1
 
-            numeric_matrix_2 = np.zeros((8, 8), dtype=np.uint16)
-            for j in range(1000):
-                perm = list(map(int, f.readline()[:-1].split()))
-                for k in range(8):
-                    numeric_matrix_2[k][perm[k]-1] += 1
+            #numeric_matrix_2 = np.zeros((8, 8), dtype=np.uint32)
+            #for j in range(1000):
+            #    perm = list(map(int, f.readline()[:-1].split()))
+            #    for k in range(8):
+            #        numeric_matrix_2[k][perm[k]-1] += 1
             
-            good = 0
-            stupid = 0
-            for k in range(8):
-                standard_deviation_1 = np.std(numeric_matrix_1[k])
-                standard_deviation_2 = np.std(numeric_matrix_2[k])
-                if abs(standard_deviation_1 - standard_deviation_2) > 1:
-                    if standard_deviation_1 < standard_deviation_2:
-                        good += 1
-                    else:
-                        stupid += 1
-                else:
-                    spread_1 = numeric_matrix_1.max() - numeric_matrix_1.min()
-                    spread_2 = numeric_matrix_2.max() - numeric_matrix_2.min()
-                    if spread_1 < spread_2:
-                        good += 1
-                    else:
-                        stupid += 1
-            
-            if good > stupid:
+            sum_anomaly_1 = (numeric_matrix_1[0][7] + numeric_matrix_1[1][0] + numeric_matrix_1[2][1] +
+                             numeric_matrix_1[3][2] + numeric_matrix_1[4][3] + numeric_matrix_1[5][4] +
+                             numeric_matrix_1[6][5] + numeric_matrix_1[7][6])
+            #sum_anomaly_2 = (numeric_matrix_2[0][7] + numeric_matrix_2[1][0] + numeric_matrix_2[2][1] +
+            #                 numeric_matrix_2[3][2] + numeric_matrix_2[4][3] + numeric_matrix_2[5][4] +
+            #                 numeric_matrix_2[6][5] + numeric_matrix_2[7][6])
+            #x = numeric_matrix_2[0]
+            #if (sum_anomaly_1 > sum_anomaly_2):
+            #    good_sets.append(i)
+            #    stupid_sets.append(i+1)
+            #else:
+            #    stupid_sets.append(i)
+            #    good_sets.append(i+1)
+            if sum_anomaly_1 <= 1115: # выявлено экспериментально, граница где-то у 1115-1131
                 good_sets.append(i)
-                stupid_sets.append(i+1)
-            elif good < stupid:
-                stupid_sets.append(i)
-                good_sets.append(i+1)
             else:
-                if abs(standard_deviation_1 - standard_deviation_2) > 1:
-                    if standard_deviation_1 < standard_deviation_2:
-                        good_sets.append(i)
-                        stupid_sets.append(i+1)
-                    else:
-                        stupid_sets.append(i)
-                        good_sets.append(i+1)
-                else:
-                    spread_1 = numeric_matrix_1.max() - numeric_matrix_1.min()
-                    spread_2 = numeric_matrix_2.max() - numeric_matrix_2.min()
-                    if spread_1 < spread_2:
-                        good_sets.append(i)
-                        stupid_sets.append(i+1)
-                    else:
-                        stupid_sets.append(i)
-                        good_sets.append(i+1)
+                stupid_sets.append(i)
 
-
+        #x1 = max(good_sets)
+        #x2 = min(good_sets)
+        #y1 = max(stupid_sets)
+        #y2 = min(stupid_sets)
         file_path_out = os.path.abspath("") + '\\2023\\ml\\Средние\\Перестановки\\output.txt'
         with open(file_path_out, "w") as f:
             for result in good_sets:
@@ -98,5 +79,29 @@ def StupidPermutation():
         perm[t1], perm[t2] = perm[t2], perm[t1]
     return perm
 
+def WriteExample(path):
+    """
+        Предназначено для выяления аномалий, записывается 1кк точек.
+        На основании них был выявлен сильный перекос, а именно, то, что
+        0 редко находится на месте 7
+        1 редко находится на месте 0
+        2 редко находится на месте 1
+        и т.д.
+    """
+    with open(path, "w") as f:
+        n = 1
+        print(n, file=f)
+        for i in range(n):
+            print(i)
+            for j in range(1000000):
+                array_random = RandomPermutation()
+                s = " ".join(str(x) for x in array_random)
+                print(s, file=f)
+            for j in range(1000000):
+                array_random = StupidPermutation()
+                s = " ".join(str(x) for x in array_random)
+                print(s, file=f)
+
 if __name__ == '__main__':
+    ## !!!!!!!!! создать самому эти выборки, и посмотреть, какая, куда и как смещена обычно? !!!!!!!!
 	main()
