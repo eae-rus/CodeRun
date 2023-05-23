ef3f4a14228a1164220a696ce30b1161cec2c105
eae_rus <eae_rus@mail.ru>
2023-05-23 22:14:58 +0300

    Изменена "Лента рекомендаций"
    Переписал в чистый массив, время особо не изменилось, теперь всего 344 вместо 341, но это мелочи.

--- 2023/ml/Сложные/Лента рекомендаций.py
+++ 2023/ml/Сложные/Лента рекомендаций.py
@@ -7,18 +7,18 @@ def main():
     '''
     n, m = list(map(int, sys.stdin.readline().strip().split()))
 
-    dict_r_a = defaultdict(list)
-    
     # Считывание данных
     array_a = []
     for i in range(n):
         array_a.append(int(sys.stdin.readline().strip()))
         
     m_deque = deque(set(array_a))
+    r_a = [[] for i in range(m)]
 
     # Разделение данных в словарь в сортированном порядке
-    for i, ai in enumerate(reversed(array_a)):
-        dict_r_a[ai].append(i)
+    for i in range(n):
+        ai = array_a[-1-i]
+        r_a[ai].append(i)
 
     # формирование выходного массива
     recomendation = []
@@ -28,16 +28,16 @@ def main():
         key_a_max = -1
         for key in m_deque:
             if previous_key != key:
-                if dict_r_a[key][-1] > a_max:
-                    a_max = dict_r_a[key][-1]
+                if r_a[key][-1] > a_max:
+                    a_max = r_a[key][-1]
                     key_a_max = key
 
         if key_a_max == -1:
             break
 
-        recomendation.append(n-1 - dict_r_a[key_a_max].pop()) # n-1 так как у нас введена обратная нумерация "ценности"
+        recomendation.append(n-1 - r_a[key_a_max].pop()) # n-1 так как у нас введена обратная нумерация "ценности"
         previous_key = key_a_max
-        if not dict_r_a[key_a_max]:
+        if len(r_a[key_a_max]) == 0:
             m_deque.remove(key_a_max)
 
     print(*recomendation) # вывод в обратном порядке
