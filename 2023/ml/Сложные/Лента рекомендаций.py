8e2ccebebbb759f2f8b469f3b82652fe6f499308
eae_rus <eae_rus@mail.ru>
2023-05-23 21:56:16 +0300

    Изменена "Kента рекомендаций"
    Переписана на немного другие функции по рекомендациям ChatGPT

--- 2023/ml/Сложные/Лента рекомендаций.py
+++ 2023/ml/Сложные/Лента рекомендаций.py
@@ -1,24 +1,22 @@
+import sys
+from collections import defaultdict
+
 def main():
     '''
     '''
-    n, m = list(map(int, input().split()))
-    
-    dict_r_a = {}
+    n, m = list(map(int, sys.stdin.readline().strip().split()))
+
+    dict_r_a = defaultdict(list)
     
     # Считывание данных
     array_a = []
     for i in range(n):
-        array_a.append(int(input()))
-    
+        array_a.append(int(sys.stdin.readline().strip()))
+
     # Разделение данных в словарь в сортированном порядке
-    for i in range(n):
-        ai = array_a[-1-i]
-        if dict_r_a.get(ai) is None:
-            dict_r_a[ai] = []
-            dict_r_a[ai].append(i)
-        else:
-            dict_r_a[ai].append(i)
-    
+    for i, ai in enumerate(reversed(array_a)):
+        dict_r_a[ai].append(i)
+
     # формирование выходного массива
     recomendation = []
     previous_key = -1
@@ -30,17 +28,17 @@ def main():
                 if dict_r_a[key][-1] > a_max:
                     a_max = dict_r_a[key][-1]
                     key_a_max = key
-        
+
         if key_a_max == -1:
             break
-        
-        recomendation.append(n-1-dict_r_a[key_a_max].pop()) # n-1 так как у нас введена обратная нумерация "ценности"
+
+        recomendation.append(n-1 - dict_r_a[key_a_max].pop()) # n-1 так как у нас введена обратная нумерация "ценности"
         previous_key = key_a_max
-        if len(dict_r_a[key_a_max]) == 0:
+        if not dict_r_a[key_a_max]:
             del dict_r_a[key_a_max]
-    
-    print(*recomendation)
-    
+
+    print(*recomendation) # вывод в обратном порядке
+
 
 if __name__ == '__main__':
-	main()
+    main()
