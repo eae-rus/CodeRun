da80df7feb07adc6e5963f8a3bfeecc51ee87ac0
eae_rus <eae_rus@mail.ru>
2023-05-23 22:25:33 +0300

    Изменена "Лента рекомендаций"
    Развёрнут алгоритм нумерации (с максимального рейтинга, на минимальный номер)

--- 2023/ml/Сложные/Лента рекомендаций.py
+++ 2023/ml/Сложные/Лента рекомендаций.py
@@ -18,27 +18,28 @@ def main():
     # Разделение данных в словарь в сортированном порядке
     for i in range(n):
         ai = array_a[-1-i]
-        r_a[ai].append(i)
+        r_a[ai].append(n-1-i)
+
 
     # формирование выходного массива
     recomendation = []
     previous_key = -1
     for j in range(n):
-        a_max = -1
-        key_a_max = -1
+        a_min = n
+        key_a_min = -1
         for key in m_deque:
             if previous_key != key:
-                if r_a[key][-1] > a_max:
-                    a_max = r_a[key][-1]
-                    key_a_max = key
+                if r_a[key][-1] < a_min:
+                    a_min = r_a[key][-1]
+                    key_a_min = key
 
-        if key_a_max == -1:
+        if key_a_min == -1:
             break
 
-        recomendation.append(n-1 - r_a[key_a_max].pop()) # n-1 так как у нас введена обратная нумерация "ценности"
-        previous_key = key_a_max
-        if len(r_a[key_a_max]) == 0:
-            m_deque.remove(key_a_max)
+        recomendation.append(r_a[key_a_min].pop())
+        previous_key = key_a_min
+        if len(r_a[key_a_min]) == 0:
+            m_deque.remove(key_a_min)
 
     print(*recomendation) # вывод в обратном порядке
 
