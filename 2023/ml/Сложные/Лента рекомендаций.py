9e1fd6109b9e7cb1572e6b7f758da0618f606afa
eae_rus <eae_rus@mail.ru>
2023-05-24 14:12:55 +0300

    Изменена "Лента рекомендаций"
    Изменил алгоритм, теперь обрабатываются "изменения"
    
    Задача решена, это первая СЛОЖНАЯ, начало положено!

--- 2023/ml/Сложные/Лента рекомендаций.py
+++ 2023/ml/Сложные/Лента рекомендаций.py
@@ -9,35 +9,58 @@ def main():
     array_a = []
     for i in range(n):
         array_a.append(int(sys.stdin.readline().strip()))
-        
-    m_set = set(array_a)
-    r_a = [[] for i in range(m)]
 
-    # Разделение данных в словарь в сортированном порядке
-    for i in range(1,n+1):
-        ai = array_a[-i]
-        r_a[ai].append(n-i)
+    # регистрация массива изменений
+    # FIXME: исправить "первый" элемент
+    # массив изменений. 
+    # Структура:
+    # - тип переменной
+    # - индекс изменения (первого вхождения)
+    # - колличество переменных
+    changes_a = [[array_a[-1], []]] 
+    len_changes = 1
+    for i in range(1,n):
+        if array_a[-i] != array_a[-i-1]:
+            changes_a[len_changes-1][1].append(n-i)
 
+            changes_structure = [array_a[-i-1], []]
+            changes_a.append(changes_structure)
+            len_changes += 1
+        else:
+            changes_a[len_changes-1][1].append(n-i)
+    # внесение последнего, 0-го, элемента (вероятно косяк алгоритма выше)
+    changes_a[len_changes-1][1].append(0)
 
-    # формирование выходного массива
+    # рекомендательный алгоритм
     recomendation = []
     previous_key = -1
-    for _ in range(n):
-        a_min = n
-        key_a_min = -1
-        for key in m_set:
-            if r_a[key][-1] < a_min:
-                if previous_key != key:
-                    a_min = r_a[key][-1]
-                    key_a_min = key
+    i = 1
+    while i <= len_changes and len_changes > 0:
+        if len_changes >= 2:
+            if changes_a[-2][0] == changes_a[-1][0]:
+                change = changes_a.pop()
+                len_changes -= 1
+                for x in change[1]:
+                    changes_a[-1][1].append(x)
 
-        if key_a_min == -1:
-            break
-
-        recomendation.append(r_a[key_a_min].pop())
-        previous_key = key_a_min
-        if r_a[key_a_min] == []:
-            m_set.discard(key_a_min)
+        if changes_a[-i][0] != previous_key:
+            recomendation.append(changes_a[-i][1].pop())
+            previous_key = changes_a[-i][0]
+            if i == 1:
+                if changes_a[-1][1] == []:
+                    changes_a.pop()
+                    len_changes -= 1 
+            elif i == 2:
+                if changes_a[-2][1] == []:
+                    change = changes_a.pop()
+                    changes_a.pop()
+                    changes_a.append(change)
+                    len_changes -= 1
+            i = 1
+            # FIXME: добавить "склеивание" для защиты от более дальних обнулений
+        else:
+            i += 1
+        
 
     print(*recomendation) # вывод в обратном порядке
 
