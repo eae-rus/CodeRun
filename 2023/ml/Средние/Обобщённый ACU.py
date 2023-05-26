902a128846b302c04d19118c052ba09358bfad6d
eae_rus <eae_rus@mail.ru>
2023-05-26 17:23:55 +0300

    Изменена "Обобщённый AUC"
    Добавил использование библиотек с сортировкой внутри библиотек. Улучшение есть, с 90 до 67мс. Но... По времени всё равно не прохожу, по ходу, там очень много разных типов.

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -13,41 +13,37 @@ def main():
     # Сортировка массивов y_true и y_pred по возрастанию y_true
     y_true_set = sorted(set(y_true))
     dict_pred = {}
-    new_y_true = []
-    new_y_pred = []
     for i in range(sample_size):
         if dict_pred.get(y_true[i]) == None:
             dict_pred[y_true[i]] = []
             dict_pred[y_true[i]].append(y_pred[i])
         else:
             dict_pred[y_true[i]].append(y_pred[i])
-
+    
+    # сортировка внутри словаря
     for y_true_sample in y_true_set:
-        for y_pred_sample in dict_pred[y_true_sample]:
-            new_y_true.append(y_true_sample)
-            new_y_pred.append(y_pred_sample)
-                
-        # TODO: Добавить сортировку по pred, и потом использовать это в следующем массиве
+        dict_pred[y_true_sample] = sorted(dict_pred[y_true_sample])
 
-    # Итерация по отсортированным массивам с двумя указателями
-    left, right = 0, 1
+    # вычисление ROC AUC
     sum_numerator = 0
     sum_divisor = 0
-    while right < sample_size:
-        if new_y_true[right] != new_y_true[left]:
-            # Вычисление числителя и знаменателя для пары left-right
-            divisor = (right - left) * (sample_size - right)
-            numerator = 0
-            for i in range(left, right):
-                for j in range(right, sample_size):
-                    if new_y_pred[i] < new_y_pred[j]:
-                        numerator += 1
-                    elif new_y_pred[i] == new_y_pred[j]:
-                        numerator += 0.5
-            sum_divisor += divisor
-            sum_numerator += numerator
-            left = right
-        right += 1
+    for y_true_left in range(y_true_set.__len__()-1):
+        name_y_true_left = y_true_set[y_true_left]
+        for y_true_right in range(y_true_left + 1, y_true_set.__len__()):
+            name_y_true_right = y_true_set[y_true_right]
+            array_left = dict_pred[name_y_true_left]
+            array_right = dict_pred[name_y_true_right]
+            len_array_left = array_left.__len__()
+            len_array_right = array_right.__len__()
+
+            sum_divisor += len_array_left * len_array_right
+            for left in range(len_array_left):
+                for right in range(len_array_right):
+                    if array_left[left] < array_right[right]:
+                        sum_numerator += len_array_right - right
+                        break
+                    elif array_left[left] == array_right[right]:
+                        sum_numerator += 0.5
 
     # Вычисление AUC
     if sum_divisor == 0:
