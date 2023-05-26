63e918f6a2c0fe21299186740d10b42d2401a345
eae_rus <eae_rus@mail.ru>
2023-05-26 20:32:50 +0300

    Изменена "Обобщённый AUC"
    Мелкие правки

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -11,30 +11,30 @@ def main():
         y_pred.append(y_pred_sample)
 
     # Сортировка массивов y_true и y_pred по возрастанию y_true
-    y_true_set = sorted(set(y_true))
+    y_true_set = list(set(y_true))
+    y_true_set.sort()
     dict_pred = {}
     for i in range(sample_size):
-        if dict_pred.get(y_true[i]) == None:
+        if y_true[i] not in dict_pred:
             dict_pred[y_true[i]] = []
-            dict_pred[y_true[i]].append(y_pred[i])
-        else:
-            dict_pred[y_true[i]].append(y_pred[i])
+        dict_pred[y_true[i]].append(y_pred[i])
     
     # сортировка внутри словаря
     for y_true_sample in y_true_set:
-        dict_pred[y_true_sample] = sorted(dict_pred[y_true_sample])
+        dict_pred[y_true_sample].sort()
 
     # вычисление ROC AUC
     sum_numerator = 0
     sum_divisor = 0
-    for y_true_left in range(y_true_set.__len__()-1):
+    len_y_true_set = len(y_true_set)
+    for y_true_left in range(len_y_true_set-1):
         name_y_true_left = y_true_set[y_true_left]
-        for y_true_right in range(y_true_left + 1, y_true_set.__len__()):
+        for y_true_right in range(y_true_left + 1, len_y_true_set):
             name_y_true_right = y_true_set[y_true_right]
             array_left = dict_pred[name_y_true_left]
             array_right = dict_pred[name_y_true_right]
-            len_array_left = array_left.__len__()
-            len_array_right = array_right.__len__()
+            len_array_left = len(array_left)
+            len_array_right = len(array_right)
 
             sum_divisor += len_array_left * len_array_right
             right_previous = 0
