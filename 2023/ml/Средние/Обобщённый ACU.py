d44fa22313624c8b88a3db8b68d10f17e2d0653c
eae_rus <eae_rus@mail.ru>
2023-05-27 11:27:33 +0300

    Изменена "Обобщённый AUC"
    Откатился назад, убрал излишние сортировки.

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -4,51 +4,45 @@ def main():
     Вроде бы с горем-пополам, разобрался...
     '''
     sample_size = int(input())
-    y_true, y_pred = [], []
+    dict_pred = {}
+    dict_pred_len = {}
+    unic_name_dict_pred = []
     for i in range(sample_size):
         y_true_sample, y_pred_sample  = map(float, input().split())
-        y_true.append(y_true_sample)
-        y_pred.append(y_pred_sample)
-
-    # Сортировка массивов y_true и y_pred по возрастанию y_true
-    y_true_set = list(set(y_true))
-    y_true_set.sort()
-    len_y_true_set = len(y_true_set)
-    dict_pred = [[] for i in range(len_y_true_set)]
-    for i in range(sample_size):
-        y_true_index = y_true_set.index(y_true[i])
-        dict_pred[y_true_index].append(y_pred[i])
-    
-    len_dict_pred = []
-    # сортировка внутри словаря
-    for y_true_sample in range(len_y_true_set):
-        dict_pred[y_true_sample].sort()
-        len_dict_pred.append(len(dict_pred[y_true_sample]))
+        if y_true_sample not in dict_pred:
+            dict_pred[y_true_sample] = []
+            dict_pred_len[y_true_sample] = 0
+            unic_name_dict_pred.append(y_true_sample)
+        dict_pred[y_true_sample].append(y_pred_sample)
+        dict_pred_len[y_true_sample] += 1
 
     # вычисление ROC AUC
     sum_numerator = 0
     sum_divisor = 0
-    left_for_divisor = 0
-    
-    for y_true_left in range(len_y_true_set-1):
-        left_for_divisor += len_dict_pred[y_true_left]
-        sum_divisor += len_dict_pred[y_true_left] * (sample_size - left_for_divisor)
-        for y_true_right in range(y_true_left + 1, len_y_true_set):      
-            right_previous = 0
-            for left in range(len_dict_pred[y_true_left]):
-                is_first_occurrence = True
-                for right in range(right_previous, len_dict_pred[y_true_right]):
-                    if dict_pred[y_true_left][left] < dict_pred[y_true_right][right]:
-                        sum_numerator += len_dict_pred[y_true_right] - right
-                        if is_first_occurrence:
-                            right_previous = right
-                            is_first_occurrence = False
-                        break
-                    elif dict_pred[y_true_left][left] == dict_pred[y_true_right][right]:
-                        sum_numerator += 0.5
-                        if is_first_occurrence:
-                            right_previous = right
-                            is_first_occurrence = False
+    len_unic_name_dict_pred = len(unic_name_dict_pred)
+    while len_unic_name_dict_pred > 1:
+        name_one = unic_name_dict_pred.pop()
+        len_unic_name_dict_pred -= 1
+        array_one = dict_pred[name_one]
+        
+        for name_two in unic_name_dict_pred:
+            array_two = dict_pred[name_two]
+            sum_divisor += dict_pred_len[name_one] * dict_pred_len[name_two]
+
+            if name_one < name_two:
+                for i in range(dict_pred_len[name_one]):
+                    for j in range(dict_pred_len[name_two]):
+                        if array_one[i] < array_two[j]:
+                            sum_numerator += 1
+                        elif array_one[i] == array_two[j]:
+                            sum_numerator += 0.5
+            else: # name_one > name_two, вариант "==" невозможен
+                for i in range(dict_pred_len[name_one]):
+                    for j in range(dict_pred_len[name_two]):
+                        if array_one[i] > array_two[j]:
+                            sum_numerator += 1
+                        elif array_one[i] == array_two[j]:
+                            sum_numerator += 0.5
 
     # Вычисление AUC
     if sum_divisor == 0:
