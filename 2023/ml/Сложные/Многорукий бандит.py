f2c51003cec48f16d73559dd47ea66174b93a8ec
eae_rus <eae_rus@mail.ru>
2023-06-07 21:30:45 +0300

    Изменена "Многорукий бандит"
    Теперь пропала "ошибка представления", и задача прост не проходит по правильности решения. Будем пробовать далее.

--- 2023/ml/Сложные/Многорукий бандит.py
+++ 2023/ml/Сложные/Многорукий бандит.py
@@ -4,38 +4,41 @@ import numpy as np
 def main():
     '''
     '''
-    n, m = map(int, input().split())
-    alpha, beta = map(float, input().split())
-    
-    # Инициализируем количество успехов и неудач для каждого автомата
-    successes = np.full(m, alpha)
-    failures = np.full(m, beta)
-    
-    # Инициализируем априорные вероятности для каждого автомата
-    prob = np.zeros(m)
-    for j in range(m):
-        prob[j] = successes[j]/ (successes[j] + failures[j])
-    prob = prob/prob.sum()
-    
-    for i in range(n):
-        # Выбираем автомат на основе текущих вероятностей
-        chosen_machine = np.random.choice(m, p=prob)
-        print(chosen_machine + 1)
+    successes, failures = [], []
+    while True:
+        n, m = map(int, input().split())
+        if n == 0 and m == 0:
+            break
         sys.stdout.flush()
-        # Получаем результат броска
-        result = int(input())
-        # Обновляем количество успехов и неудач для выбранного автомата
-        if result == 1:
-            successes[chosen_machine] += 1
-        else:
-            failures[chosen_machine] += 1
-        # Обновляем вероятности для каждого автомата на основе результатов
+        alpha, beta = map(float, input().split())
+
+        # Инициализируем количество успехов и неудач для каждого автомата
+        if successes == []:
+            successes = np.full(m, alpha)
+            failures = np.full(m, beta)
+
+        # Инициализируем априорные вероятности для каждого автомата
+        prob = np.zeros(m)
         for j in range(m):
             prob[j] = successes[j]/ (successes[j] + failures[j])
         prob = prob/prob.sum()
-    
-    sys.stdout.flush() # быть может нужно...?
-    n_zero, m_zero = map(int, input().split())
+
+        for i in range(n):
+            # Выбираем автомат на основе текущих вероятностей
+            chosen_machine = np.random.choice(m, p=prob)
+            print(chosen_machine + 1)
+            sys.stdout.flush()
+            # Получаем результат броска
+            result = int(input())
+            # Обновляем количество успехов и неудач для выбранного автомата
+            if result == 1:
+                successes[chosen_machine] += 1
+            else:
+                failures[chosen_machine] += 1
+            # Обновляем вероятности для каждого автомата на основе результатов
+            for j in range(m):
+                prob[j] = successes[j]/ (successes[j] + failures[j])
+            prob = prob/prob.sum()
 
 if __name__ == '__main__':
     main()
