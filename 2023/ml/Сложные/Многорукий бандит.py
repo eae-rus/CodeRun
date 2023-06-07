434f49f1cd40c1ae0ad8f5dac030bf8ab4a918be
eae_rus <eae_rus@mail.ru>
2023-06-07 20:45:46 +0300

    + Добавлено начало "Многорукий бандит"
    Чёт ChatGPT давала мне по началу сильно левое решение, с трудом перевёл на что-то близкое. (хотя костыль давала хороший, но делающий совсем не то). Сейчас запущу тест, посмотрим на результаты.
    
    Хмм... Presentation error

---
+++ 2023/ml/Сложные/Многорукий бандит.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+
+def main():
+    '''
+    '''
+    n, m = map(int, input().split())
+    alpha, beta = map(float, input().split())
+    
+    # Инициализируем количество успехов и неудач для каждого автомата
+    successes = np.full(m, alpha)
+    failures = np.full(m, beta)
+    
+    # Инициализируем априорные вероятности для каждого автомата
+    prob = np.zeros(m)
+    for j in range(m):
+        prob[j] = successes[j]/ (successes[j].sum() + failures[j].sum()) 
+    
+    for i in range(n):
+        # Выбираем автомат на основе текущих вероятностей
+        chosen_machine = np.random.choice(m, p=prob)
+        print(chosen_machine + 1)
+        sys.stdout.flush()
+        # Получаем результат броска
+        result = int(input())
+        # Обновляем количество успехов и неудач для выбранного автомата
+        if result == 1:
+            successes[chosen_machine] += 1
+        else:
+            failures[chosen_machine] += 1
+        # Обновляем вероятности для каждого автомата на основе результатов
+        for j in range(m):
+            prob[j] = successes[j]/ (successes[j] + failures[j])
+        prob = prob/prob.sum()
+
+if __name__ == '__main__':
+    main()
