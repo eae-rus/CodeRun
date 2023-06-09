295e72ba51a4e0205c5220d1d047b6c69a62ee14
eae_rus <eae_rus@mail.ru>
2023-06-09 22:56:02 +0300

    Изменена "Безградиентная оптимизация функции"
    Как мне позже подсказала ChatGPT - я реализовывал принцип "слепого поиска", ещё и не самым оптимальным методом...
    Она же мне помогла реализовать через "метод отжига", ничего сверх-сложного нет, и да, мой алгоритм не лучший был.

--- 2023/ml/Средние/Безградиентная оптимизация функции.py
+++ 2023/ml/Средние/Безградиентная оптимизация функции.py
@@ -1,54 +1,50 @@
 import sys
+import random
+import math
 
 def main():
     '''
+    Реализиую метод симуляции отжига
     '''
-    X, Y, Z = map(float, input().split())
-    number_turns = 0
-    step = 1
-    itteration = 0
-    X_previous_sign = 1
-    Y_previous_sign = 1    
+    x, y, h = map(float, input().split())
+    
+    T = 1 # Температура отжига
+    alpha = 0.975 # коэффициент трения отжига (0.975^200 примерно 0.01)
     
     while True:
-        itteration += 1
-        if number_turns == 0:
-            print(X + step*X_previous_sign, Y, itteration)
-        elif number_turns == 1:
-            print(X, Y + step*Y_previous_sign, itteration)
-        elif number_turns == 2:
-            print(X - step*X_previous_sign, Y, itteration)
-        elif number_turns == 3:
-            print(X, Y - step*Y_previous_sign, itteration)
-        else:
-            step /= 2
-            number_turns = 0
+        # Выбор случайного направления
+        dx = random.uniform(-1, 1)
+        dy = random.uniform(-1, 1)
+        
+        # Нормировка направления
+        norm = math.sqrt(dx*dx + dy*dy)
+        dx /= norm
+        dy /= norm
+        
+        # Вычисление новых координат
+        x_new = x + dx*T
+        y_new = y + dy*T
+        
+        # Проверка на границы допустимых значений
+        if x_new < -50 or x_new > 100 or y_new < -50 or y_new > 100:
             continue
         
+        print(x_new, y_new)
         sys.stdout.flush()
-        sign, Z_new = input().split()
+        sign, h_new = input().split()
         
-        if sign == "+" or itteration > 205:
-            break
+        if sign == "+":
+            exit(0)
         
-        Z_new = float(Z_new)
-        if Z_new > Z:
-            Z = Z_new
-            number_turns = 0
-            if number_turns == 0:
-                X += step
-                X_previous_sign = 1
-            elif number_turns == 1:
-                Y += step
-                Y_previous_sign = 1
-            elif number_turns == 2:
-                X -= step
-                X_previous_sign = -1
-            else:
-                Y -= step
-                Y_previous_sign = -1
-        else:
-            number_turns += 1
+        h_new = float(h_new)
+        if h_new > h:
+            h = h_new
+            x = x_new
+            y = y_new
+        
+        # Обновление температуры
+        T *= alpha
+        
 
 if __name__ == '__main__':
     main()
