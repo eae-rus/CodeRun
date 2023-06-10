8404d830720fdd9cecf9dc5d7142a4f036856532
eae_rus <eae_rus@mail.ru>
2023-06-10 22:14:33 +0300

    Изменена "Обобщённый AUC"
    Последняя реализация модернизирована с учётом рекомендация ChatGPT (по сути, содержание то же осталось)
    
    Не прокатило...

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -1,31 +1,46 @@
+import numpy as np
+
 def main():
-    # Читаем входные данные
-    n = int(input())
-    data = []
-    for i in range(n):
-        t, y = map(float, input().split())
-        data.append((t, y))
+    '''
+    Сперва бы разобраться, как это вообще считается...
+    Вроде бы с горем-пополам, разобрался...
+    '''
+    sample_size = int(input())
+    y_true, y_pred = np.zeros(sample_size), np.zeros(sample_size)
+    for i in range(sample_size):
+        y_true[i], y_pred[i]  = map(float, input().split())
 
-    # Сортируем массив предсказаний модели по возрастанию
-    data = sorted(data, key=lambda x: x[1])
+    sample_size = len(y_true)
+    dict_pred = {}
+    unic_name_dict_pred = np.unique(y_true)
+    for i, name in enumerate(unic_name_dict_pred):
+        dict_pred[name] = y_pred[np.where(y_true == name)[0]]
+        dict_pred[name].sort()
+    sum_numerator = 0
+    sum_divisor = 0
+    left_for_divisor = 0
+    for i in range(len(unic_name_dict_pred)):
+        len_array_left = len(dict_pred[unic_name_dict_pred[i]])
+        left_for_divisor += len_array_left
+        sum_divisor += len_array_left * (sample_size - left_for_divisor)
+        array_i = dict_pred[unic_name_dict_pred[i]]
+        for j in range(i + 1, len(unic_name_dict_pred)):
+            len_array_right = len(dict_pred[unic_name_dict_pred[j]])
+            array_j = dict_pred[unic_name_dict_pred[j]]
+            right = np.searchsorted(array_j, array_i)
+            for index in range(len(right)):
+                if right[index] == len_array_right:
+                    continue
+                if array_i[index] != array_j[right[index]]:
+                    sum_numerator += len_array_right - right[index]
+                else:
+                    while right[index] < len_array_right and array_i[index] == array_j[right[index]]:
+                        sum_numerator += 0.5
+                        right[index] += 1
+                    sum_numerator += len_array_right - right[index]
+    auc = sum_numerator / sum_divisor
 
-    # Считаем количество пар объектов (i, j), где i < j и y_i > y_j
-    count = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            if data[i][0] > data[j][0]:
-                count += 1
-            elif data[i][0] == data[j][0]:
-                count += 0.5
-
-    # Вычисляем AUC
-    if count == 0:
-        auc = 0.0
-    else:
-        auc = count / (n * (n - 1) / 2)
-
-    # Выводим результат
-    print('{:.6f}'.format(auc))
+    print(auc)
 
 if __name__ == '__main__':
     main()
