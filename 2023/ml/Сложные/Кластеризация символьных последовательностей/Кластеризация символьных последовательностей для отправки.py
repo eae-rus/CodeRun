f08e218d0b09229211ff8a72d49bac0448915c4b
eae_rus <eae_rus@mail.ru>
2023-06-14 07:22:50 +0300

    Изменена "Кластеризация символьных последовательностей"
    Представление через вероятности перехода от одного символа к другому.

--- 2023/ml/Сложные/Кластеризация символьных последовательностей/Кластеризация символьных последовательностей для отправки.py
+++ 2023/ml/Сложные/Кластеризация символьных последовательностей/Кластеризация символьных последовательностей для отправки.py
@@ -20,19 +20,33 @@ def main():
             X_max_len = len(string)
         X.append([char_to_index[char] for char in string])
         
-    X_new = np.zeros((n, k-1 + (k-1)*(k-1) + k-1 + 1), dtype=np.float32)
+    X_new = np.zeros((n, k*(k+1) + 1), dtype=np.float32)
     for i in range(n):
         x_len = len(X[i])
+        X_local = np.zeros((k, k+1), dtype=np.float32)
+        
         for j in range(x_len):
             char_index_j = X[i][j]
             if j == 0:
-                X_new[i, char_index_j] = 1
+                X_local[0, char_index_j] = 1
             if j > 0:
                 char_index_j_1 = X[i][j-1]
-                X_new[i, k-1 + char_index_j_1+(k-1) + char_index_j] += 1 / (x_len-1)
+                X_local[char_index_j_1, char_index_j] += 1
                 
             if j == len(X[i]) - 1:
-                X_new[i, k-1 + (k-1)*(k-1) + char_index_j] = 1
+                X_local[char_index_j, k] = 1
+                
+        for j in range(k):
+            sum_x_j = X_local[j, :].sum() + 1
+            for k in range(k+1):
+                if X_local[j, k] > 0:
+                    X_local[j, k] /= sum_x_j
+                else:
+                    X_local[j, k] = -1
+        for j in range(k):
+            for z in range(k+1):
+                X_new[i, j*(k+1) + z] = X_local[j, z]
+            
         X_new[i, -1] = x_len / X_max_len
 
     
