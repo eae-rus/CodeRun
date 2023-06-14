c0f26f4252d65e551226126492a6098332e2281c
eae_rus <eae_rus@mail.ru>
2023-06-15 01:04:34 +0300

    Изменена "Обобщённый AUC" v4
    Вроде бы реализовал итоговую версию "разделя и влавствуй", ну сейчас посмотрим... Да блин, опять лимит времени...

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -4,82 +4,74 @@ def main():
     Вроде бы с горем-пополам, разобрался...
  
     Если это заработает - то будет чудом))
+    Надеюсь всё же доразделялся))
     '''
-    def calculate_numerator(array_1, start_1, end_1, array_2, start_2, end_2):
-        if end_1 - start_1 < 1 and end_2 - start_2 < 1:
-            return 0
-        elif end_1 - start_1 <= 1 and end_2 - start_2 <= 1:
-            if array_1[start_1] < array_2[start_2]:
-                return 1
-            elif array_1[start_1] == array_2[start_2]:
-                return 0.5
-            else:
-                return 0
+    def calculate_auc(y_true, y_pred, start, end):
+        if end - start < 1:
+            return 0, 0
+        elif end - start == 1:
+            if y_true[start] < y_true[end]:
+                if y_pred[start] < y_pred[end]:
+                    return 1, 1
+                elif y_pred[start] == y_pred[end]:
+                    return 0.5, 1
+                else:
+                    return 0, 1
+            elif y_true[start] == y_true[end]:
+                return 0, 0
+            else: # y_true[start] > y_true[end]
+                if y_pred[start] > y_pred[end]:
+                    return 1, 1
+                elif y_pred[start] == y_pred[end]:
+                    return 0.5, 1
+                else:
+                    return 0, 1
 
-        mid_1 = (start_1 + end_1) // 2
-        mid_2 = (start_2 + end_2) // 2
+        mid = (start + end) // 2
 
-        left_numerator = calculate_numerator(array_1, start_1, mid_1, array_2, start_2, mid_2)
-        right_numerator = calculate_numerator(array_1, mid_1, end_1, array_2, mid_2, end_2)
+        left_numerator, left_divisor = calculate_auc(y_true, y_pred, start, mid)
+        right_numerator, right_divisor = calculate_auc(y_true, y_pred, mid, end)
 
         numerator = left_numerator + right_numerator
-
-        if end_1 - start_1 > 1 and end_2 - start_2 > 1:
-            for i in range(start_1, mid_1):
-                for j in range(mid_2, end_2):
-                    if array_1[i] < array_2[j]:
+        divisor = left_divisor + right_divisor
+        
+        for i in range(start, mid):
+            for j in range(mid+1, end+1):
+                if y_true[i] < y_true[j]:
+                    if y_pred[i] < y_pred[j]:
                         numerator += 1
-                    elif array_1[i] == array_2[j]:
+                        divisor += 1
+                    elif y_pred[i] == y_pred[j]:
                         numerator += 0.5
-
-            for i in range(mid_1, end_1):
-                for j in range(start_2, mid_2):
-                    if array_1[i] < array_2[j]:
+                        divisor += 1
+                    else: # y_pred[i] > y_pred[j]
+                        numerator += 0
+                        divisor += 1
+                elif y_true[i] == y_true[j]:
+                    continue
+                else: # y_true[i] > y_true[j]
+                    if y_pred[i] > y_pred[j]:
                         numerator += 1
-                    elif array_1[i] == array_2[j]:
+                        divisor += 1
+                    elif y_pred[i] == y_pred[j]:
                         numerator += 0.5
+                        divisor += 1
+                    else: # y_pred[i] < y_pred[j]
+                        numerator += 0
+                        divisor += 1
 
-        return numerator
+        return numerator, divisor
     
     
     sample_size = int(input())
-    dict_pred = {}
-    dict_pred_len = {}
-    unic_name = []
+    y_true, y_pred = [0] * sample_size, [0] * sample_size
     for i in range(sample_size):
-        y_true_sample, y_pred_sample  = map(float, input().split())
-        if y_true_sample not in dict_pred:
-            dict_pred[y_true_sample] = []
-            dict_pred_len[y_true_sample] = 0
-            unic_name.append(y_true_sample)
-        dict_pred[y_true_sample].append(y_pred_sample)
-        dict_pred_len[y_true_sample] += 1
-
-    pred = []
-    pred_len = []
-    unic_name.sort()
-    # Создание единого массива
-    for name in unic_name:
-        for x in dict_pred[name]:
-            pred.append(x)
-        pred_len.append(dict_pred_len[name])
+        y_true_sample, y_pred_sample = map(float, input().split())
+        y_true[i] = y_true_sample
+        y_pred[i] = y_pred_sample
 
     # вычисление ROC AUC
-    sum_numerator = 0
-    sum_divisor = 0
-    
-    coordinates = 0
-    for iteration in range(len(pred_len)-1):
-        coordinates += pred_len[iteration]
-        sum_divisor += pred_len[iteration] * (sample_size - coordinates)
-        sum_numerator += calculate_numerator(pred[coordinates-pred_len[iteration]: coordinates], 0, pred_len[iteration],
-                                             pred[coordinates: sample_size], 0, sample_size - coordinates)
-        #for i in range(coordinates-pred_len[iteration], coordinates):
-        #    for j in range(coordinates, sample_size):
-        #        if pred[i] == pred[j]:
-        #            sum_numerator += 0.5
-        #        elif pred[i] < pred[j]:
-        #            sum_numerator += 1
+    sum_numerator, sum_divisor = calculate_auc(y_true, y_pred, 0, sample_size-1)
 
     # Вычисление AUC
     auc = sum_numerator / sum_divisor
