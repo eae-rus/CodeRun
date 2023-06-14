ee8969d1b43a87016c45aa4d2de10444db09cd8f
eae_rus <eae_rus@mail.ru>
2023-06-14 21:46:08 +0300

    Изменена "Обобщённый AUC" v3
    Добавил исследование "меньших"

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -2,7 +2,55 @@ def main():
     '''
     Сперва бы разобраться, как это вообще считается...
     Вроде бы с горем-пополам, разобрался...
+ 
+    Если это заработает - то будет чудом))
     '''
+    def calculate_sum_numerator(lowwer, lowwer_len, upper, upper_len):
+        sum_numerator = 0
+        right_previous = 0
+        if lowwer_len <= upper_len:
+            for left in range(lowwer_len):
+                is_first_occurrence = True
+                if left > 0 and lowwer[left-1] == lowwer[left]:
+                    sum_numerator += local_sum_numerator
+                    continue
+                local_sum_numerator = 0
+                for right in range(right_previous, upper_len):
+                    if lowwer[left] < upper[right]:
+                        local_sum_numerator += upper_len - right
+                        if is_first_occurrence:
+                            right_previous = right
+                            is_first_occurrence = False
+                        break
+                    elif lowwer[left] == upper[right]:
+                        local_sum_numerator += 0.5
+                        if is_first_occurrence:
+                            right_previous = right
+                            is_first_occurrence = False
+                sum_numerator += local_sum_numerator
+        else:
+            for left in range(upper_len):
+                is_first_occurrence = True
+                if left > 0 and upper[-left-1] == upper[-left]:
+                    sum_numerator += local_sum_numerator
+                    continue
+                local_sum_numerator = 0
+                for right in range(right_previous, lowwer_len):
+                    if upper[-left-1] > lowwer[-right-1]:
+                        local_sum_numerator += lowwer_len - right
+                        if is_first_occurrence:
+                            right_previous = right
+                            is_first_occurrence = False
+                        break
+                    elif upper[-left-1] == lowwer[-right-1]:
+                        local_sum_numerator += 0.5
+                        if is_first_occurrence:
+                            right_previous = right
+                            is_first_occurrence = False
+                sum_numerator += local_sum_numerator
+        return sum_numerator
+    
+    
     sample_size = int(input())
     dict_pred = {}
     dict_pred_len = {}
@@ -28,41 +76,20 @@ def main():
     
     unic_name_len = len(unic_name)
     while unic_name_len > 1:
-        name_one = unic_name.pop()
+        name_upper = unic_name.pop() # идём в обратную сторону, значит name_one > name_two
         unic_name_len -= 1
         
-        left_for_divisor += dict_pred_len[name_one]
-        sum_divisor += dict_pred_len[name_one] * (sample_size - left_for_divisor)
-        for name_two in unic_name:      
-            right_previous = 0
-            for left in range(dict_pred_len[name_one]):
-                is_first_occurrence = True
-                if left > 0 and dict_pred[name_one][left-1] == dict_pred[name_one][left]:
-                    sum_numerator += local_sum_numerator
-                    continue
-                local_sum_numerator = 0
-                right = right_previous
-                while right < dict_pred_len[name_two]:
-                    if dict_pred[name_one][left] < dict_pred[name_two][right]:
-                        local_sum_numerator += dict_pred_len[name_two] - right
-                        if is_first_occurrence:
-                            right_previous = right
-                        break
-                    elif dict_pred[name_one][left] == dict_pred[name_two][right]:
-                        local_sum_numerator += 0.5
-                        if is_first_occurrence:
-                            right_previous = right
-                            is_first_occurrence = False
-                    right += 1
-                sum_numerator += local_sum_numerator
-                if right == dict_pred_len[name_two] and is_first_occurrence:
-                    break
+        left_for_divisor += dict_pred_len[name_upper]
+        sum_divisor += dict_pred_len[name_upper] * (sample_size - left_for_divisor)
+        for name_lowwer in unic_name:      
+            sum_numerator += calculate_sum_numerator(dict_pred[name_lowwer], dict_pred_len[name_lowwer],
+                                                     dict_pred[name_upper], dict_pred_len[name_upper])
 
     # Вычисление AUC
     if sum_divisor == 0:
         auc = 0.0
     else:
-        auc = 1 - sum_numerator / sum_divisor
+        auc = sum_numerator / sum_divisor
 
     print(auc)
 
