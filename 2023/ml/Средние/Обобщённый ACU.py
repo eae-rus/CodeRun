b740d6474906f815d4a75d8d3180dd1f74d5a859
eae_rus <eae_rus@mail.ru>
2023-06-15 20:41:56 +0300

    Изменена "Обобщённый AUC" v5
    Применена новая концепция "двойных сортированных словарей", посмотрим...
    
    Не зашло...

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -4,74 +4,60 @@ def main():
     Вроде бы с горем-пополам, разобрался...
  
     Если это заработает - то будет чудом))
-    Надеюсь всё же доразделялся))
+    Новая версия "двойные словари"
     '''
-    def calculate_auc(y_true, y_pred):
-        if len(y_true) < 2:
-            return 0, 0
-        elif len(y_true) == 2:
-            if y_true[0] == y_true[1]:
-                return 0, 0
-            elif y_true[0] < y_true[1]:
-                if y_pred[0] < y_pred[1]:
-                    return 1, 1
-                elif y_pred[0] == y_pred[1]:
-                    return 0.5, 1
-                else:
-                    return 0, 1
-            else: # y_true[start] > y_true[end]
-                if y_pred[0] > y_pred[1]:
-                    return 1, 1
-                elif y_pred[0] == y_pred[1]:
-                    return 0.5, 1
-                else:
-                    return 0, 1
-
-        mid = len(y_true) // 2
-
-        left_numerator, left_divisor = calculate_auc(y_true[:mid], y_pred[:mid])
-        right_numerator, right_divisor = calculate_auc(y_true[mid:], y_pred[mid:])
-
-        numerator = left_numerator + right_numerator
-        divisor = left_divisor + right_divisor
-        
-        for i in range(mid):
-            for j in range(mid, len(y_true)):
-                if y_true[i] == y_true[j]:
-                    continue
-                elif y_true[i] < y_true[j]:
-                    if y_pred[i] < y_pred[j]:
-                        numerator += 1
-                        divisor += 1
-                    elif y_pred[i] == y_pred[j]:
-                        numerator += 0.5
-                        divisor += 1
-                    else: # y_pred[i] > y_pred[j]
-                        numerator += 0
-                        divisor += 1
-                else: # y_true[i] > y_true[j]
-                    if y_pred[i] > y_pred[j]:
-                        numerator += 1
-                        divisor += 1
-                    elif y_pred[i] == y_pred[j]:
-                        numerator += 0.5
-                        divisor += 1
-                    else: # y_pred[i] < y_pred[j]
-                        numerator += 0
-                        divisor += 1
-
-        return numerator, divisor
-    
-    
+      
     sample_size = int(input())
-    y_true, y_pred = [0] * sample_size, [0] * sample_size
+    t_dict = {}
+    t_len_dict = {}
+    t_unic = []
     for i in range(sample_size):
-        y_true_sample, y_pred_sample = map(float, input().split())
-        y_true[i] = y_true_sample
-        y_pred[i] = y_pred_sample
+        t, y  = map(float, input().split())
+        if t not in t_dict:
+            t_dict[t] = {}
+            t_len_dict[t] = 0
+            t_unic.append(t)
+        if y not in t_dict[t]:
+            t_dict[t][y] = 0
+        t_dict[t][y] += 1
+        t_len_dict[t] += 1
+
+    # сортировка уникальных "t"
+    t_unic.sort()
+    # Создание словаря уникальных "y" в каждом подмасиве "t" и их сортировка
+    y_unic = {}
+    y_len_dict = {}
+    for name in t_unic:
+        y_unic_keys = list(t_dict[name].keys())
+        y_unic_keys.sort()
+        y_unic[name] = y_unic_keys
+        y_len_dict[name] = len(y_unic[name])
 
     # вычисление ROC AUC
-    sum_numerator, sum_divisor = calculate_auc(y_true, y_pred)
+    sum_numerator = 0
+    sum_divisor = 0
+    for t_low_index in range(len(t_unic)-1):
+        t_low_name = t_unic[t_low_index]
+        for t_up_index in range(t_low_index+1, len(t_unic)):
+            t_up_name = t_unic[t_up_index]
+            sum_divisor += t_len_dict[t_low_name] * t_len_dict[t_up_name]
+            
+            i, j = 0, 0
+            y_up_len_sum = 0
+            while i < y_len_dict[t_low_name] and j < y_len_dict[t_up_name]:
+                y_i = y_unic[t_low_name][i]
+                y_j = y_unic[t_up_name][j]
+                
+                if y_i == y_j:
+                    sum_numerator += 0.5 * t_dict[t_low_name][y_i] * t_dict[t_up_name][y_j]
+                    y_up_len_sum += t_dict[t_up_name][y_j]
+                    j += 1
+                elif y_i < y_j:
+                    sum_numerator += t_dict[t_low_name][y_i] * (t_len_dict[t_up_name] - y_up_len_sum)
+                    i += 1
+                else:
+                    y_up_len_sum += t_dict[t_up_name][y_j]
+                    j += 1
 
     # Вычисление AUC
     auc = sum_numerator / sum_divisor
