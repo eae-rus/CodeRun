20761d1076593b5f300f81479d118e961d4c367e
EAE_RUS <EAE_RUS@mail.ru>
2023-06-19 18:44:29 +0300

    Изменена "Обобщённый AUC" v4
    Переделал расчёт делителя. ЭХ... Почти.

--- 2023/ml/Средние/Обобщённый ACU.py
+++ 2023/ml/Средние/Обобщённый ACU.py
@@ -2,58 +2,7 @@ from collections import defaultdict
 
 def main():
     '''
-    Сперва бы разобраться, как это вообще считается...
-    Вроде бы с горем-пополам, разобрался...
- 
-    Если это заработает - то будет чудом, наконец-то свершилось!!))
-    https://www.techiedelight.com/merge-sort/
-    Но вся пляска была с тем, как исключить лишнее при подсчёте слияний.
-    Над этим было много помощи / подсказок...
     '''
-    def merge_sort_for_divisor(data):
-        if len(data) <= 1:
-            return data, 0
-        elif len(data) == 2:
-            return merge_for_divisor(data[:1], data[1:])
-        else:
-            mid = len(data) // 2
-            left, divisor_left = merge_sort_for_divisor(data[:mid])
-            right, divisor_right = merge_sort_for_divisor(data[mid:])
-            merged, divisor_merge = merge_for_divisor(left, right)
-            divisor = divisor_left + divisor_right + divisor_merge
-            return merged, divisor
-
-    def merge_for_divisor(left, right):
-        '''
-        Пока прост выкинул расчёт числителя, оптимизировать потом можно будет
-        '''
-        merged = []
-        i, j, divisor = 0, 0, 0, 0
-        equal_right = j
-        while i < len(left) and j <len(right):
-            if left[i][0] < right[j][0]:
-                merged.append(left[i])
-                divisor += len(right) - j
-                i += 1
-            elif left[i][0] == right[j][0]:
-                merged.append(left[i])
-                if equal_right < j: # счётчик, чтобы не пересчитывать
-                    equal_right = j
-                for z in range(equal_right, len(right)):
-                    if left[i][0] < right[z][0]:
-                        equal_right = z
-                        divisor += len(right) - z
-                        break             
-                i += 1
-            else: # left[i][0] > right[j][0]
-                merged.append(right[j])
-                divisor += len(left) - i
-                j += 1            
-
-        merged += left[i:]
-        merged += right[j:]
-        return merged, divisor
-
     def merge_sort_for_numerator(data):
         if len(data) <= 1:
             return data, 0
@@ -121,7 +70,15 @@ def main():
         data.append((t, y))
 
     # Сперва выравниваем по true
-    data, divisor = merge_sort_for_divisor(data)
+    data.sort(key=lambda x: x[0])
+    divisor = 0
+    repeat = 1
+    for i in range(1, len(data)):
+        if data[i][0] != data[i-1][0]:
+            divisor += repeat * (len(data) - i)
+            repeat = 1
+        else:
+            repeat += 1
     # А потом уже по predict
     data, numerator = merge_sort_for_numerator(data)
 
