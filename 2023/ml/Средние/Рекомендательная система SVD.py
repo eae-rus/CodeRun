fe10e8025126b8d62eddfc0dbf6fefb8fd86ad66
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-02 02:02:55 +0300

    Изменена "Рекомендательная система SVD"
    Добавил учёт "среднего значения", на основе этого правильнее заработала регуляризация

--- 2023/ml/Средние/Рекомендательная система SVD.py
+++ 2023/ml/Средние/Рекомендательная система SVD.py
@@ -1,15 +1,30 @@
 import random
 
 def main():
+    def dot_product(vector1, vector2):
+        return sum(v1 * v2 for v1, v2 in zip(vector1, vector2))
+
+    def update_vector(vector, updates):
+        for i, update in enumerate(updates):
+            vector[i] += update
+    
     # чтение входных данных
     k_value, U_value, M_value, D_value, T_value = map(int, input().split())
 
     U_M_train = []
+    all_ratings = 0
+    all_ratings_count = 0
     for _ in range(D_value):
         user, movie, rating = map(int, input().split())
         rating /= k_value
         # занесение данных в матрицу
         U_M_train.append((user, movie, rating))
+        # вычисления средних
+        all_ratings += rating
+        all_ratings_count += 1
+
+    # вычисляем среднее значение оценок
+    mean_rating = all_ratings / all_ratings_count
 
     min_dim = 10
 
@@ -17,19 +32,19 @@ def main():
     random.seed(42)
 
     # инициализация матриц U и V
-    P = [[random.normalvariate(0, 1./min_dim) for _ in range(min_dim)] for _ in range(U_value)]
-    Q = [[random.normalvariate(0, 1./min_dim) for _ in range(min_dim)] for _ in range(M_value)]
+    P = [[random.normalvariate(0, 1/min_dim) for _ in range(min_dim)] for _ in range(U_value)]
+    Q = [[random.normalvariate(0, 1/min_dim) for _ in range(min_dim)] for _ in range(M_value)]
 
     user_bias = [0] * U_value
     movie_bias = [0] * M_value
 
     # обучение модели SVD с регуляризацией
-    learning_rate  = 0.6
-    reg_param  = 0.2
-    for epoch in range(33):
+    learning_rate  = 0.8
+    reg_param  = 0.1
+    for epoch in range(34):
         for index in range(D_value):
             user, movie, value = U_M_train[index]
-            prediction = user_bias[user] + movie_bias[movie] + dot_product(P[user], Q[movie])
+            prediction = mean_rating + user_bias[user] + movie_bias[movie] + dot_product(P[user], Q[movie])
             error = (value - prediction)
 
             user_bias[user] += learning_rate  * (error - reg_param  * user_bias[user])
@@ -42,16 +57,10 @@ def main():
     # вычисление предсказаний и вывод результатов
     for _ in range(T_value):
         user, movie = map(int, input().split())
-        rating = k_value * (user_bias[user] + movie_bias[movie] + dot_product(P[user], Q[movie]))
+        rating = k_value * (mean_rating + user_bias[user] + movie_bias[movie] + dot_product(P[user], Q[movie]))
         rating = min(max(rating, 0), k_value)
         print(rating)
 
-def dot_product(vector1, vector2):
-    return sum(v1 * v2 for v1, v2 in zip(vector1, vector2))
-
-def update_vector(vector, updates):
-    for i, update in enumerate(updates):
-        vector[i] += update
 
 if __name__ == '__main__':
     main()
