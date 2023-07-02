6bba2aa2a6d0fb7c9d4c80726a89301ab8d8b1c2
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-02 11:39:19 +0300

    Изменена "Рекомендательная система SVD"
    Мелкий рефакторинг по сути.

--- 2023/ml/Средние/Рекомендательная система SVD.py
+++ 2023/ml/Средние/Рекомендательная система SVD.py
@@ -10,10 +10,8 @@ def main():
     
     # чтение входных данных
     k_value, U_value, M_value, D_value, T_value = map(int, input().split())
-
     U_M_train = []
-    all_ratings = 0
-    all_ratings_count = 0
+    all_ratings, all_ratings_count = 0, 0
     for _ in range(D_value):
         user, movie, rating = map(int, input().split())
         rating /= k_value
@@ -26,24 +24,21 @@ def main():
     # вычисляем среднее значение оценок
     mean_rating = all_ratings / all_ratings_count
 
-    min_dim = 10
-
     # задаем зерно для генератора случайных чисел
     random.seed(42)
 
-    # инициализация матриц U и V
+    # инициализация матриц P и Q и базисы
+    min_dim = 10
     P = [[random.normalvariate(0, 1/min_dim) for _ in range(min_dim)] for _ in range(U_value)]
     Q = [[random.normalvariate(0, 1/min_dim) for _ in range(min_dim)] for _ in range(M_value)]
-
     user_bias = [0] * U_value
     movie_bias = [0] * M_value
 
     # обучение модели SVD с регуляризацией
-    learning_rate  = 0.8
-    reg_param  = 0.1
-    for epoch in range(34):
-        for index in range(D_value):
-            user, movie, value = U_M_train[index]
+    learning_rate  = 0.005
+    reg_param  = 0.02
+    for epoch in range(33):
+        for user, movie, value in U_M_train:
             prediction = mean_rating + user_bias[user] + movie_bias[movie] + dot_product(P[user], Q[movie])
             error = (value - prediction)
 
@@ -51,8 +46,8 @@ def main():
             movie_bias[movie] += learning_rate  * (error - reg_param  * movie_bias[movie])
             update_vector(P[user], [learning_rate * (error * Q[movie][i] - reg_param * P[user][i]) for i in range(min_dim)])
             update_vector(Q[movie], [learning_rate * (error * P[user][i] - reg_param * Q[movie][i]) for i in range(min_dim)])
-        if epoch % 10 == 0:
-            learning_rate = learning_rate / 2
+        # if epoch % 5 == 0:
+        #     learning_rate = learning_rate / 2
 
     # вычисление предсказаний и вывод результатов
     for _ in range(T_value):
