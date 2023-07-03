e124b70b9e387866a4c0d17c9169e523aa35c42b
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-03 18:01:43 +0300

    Изменена "Рекомендательная система SVD"
    Убрал нормализацию, чтобы те коэффициенты давали толк.
    
    Задача решена. Это треш...

--- 2023/ml/Средние/Рекомендательная система SVD.py
+++ 2023/ml/Средние/Рекомендательная система SVD.py
@@ -5,41 +5,37 @@ def main():
     k_value, U_value, M_value, D_value, T_value = map(int, input().split())
 
     U_M_train = []
-    all_ratings = 0
+    mean_rating = 0
     for _ in range(D_value):
         user, movie, rating = map(int, input().split())
-        rating /= k_value
+        # rating /= k_value
         # занесение данных в матрицу
         U_M_train.append((user, movie, rating))
         # вычисления средних
-        all_ratings += rating
+        mean_rating += rating
 
     # вычисляем среднее значение оценок
-    mean_rating = all_ratings / D_value
+    mean_rating = mean_rating / D_value
 
-    if U_value < 5:
-        min_dim = 10
-        n_epoch  = 1000
-    else:
-        min_dim = 20
-        n_epoch = 10
+    min_dim = 20
+    n_epoch = 9
         
     # задаем зерно для генератора случайных чисел
-    np.random.seed(42)
+    # np.random.seed(42)
 
-    # инициализация матриц U и V
-    P = np.random.normal(scale=1./min_dim, size=(U_value, min_dim))
-    Q = np.random.normal(scale=1./min_dim, size=(M_value, min_dim))
+    # инициализация матриц P и Q
+    P = np.random.normal(0, 0.01, size=(U_value, min_dim))
+    Q = np.random.normal(0, 0.01, size=(M_value, min_dim))
 
     user_bias = np.zeros(U_value)
     movie_bias = np.zeros(M_value)
 
     # обучение модели SVD с регуляризацией
-    learning_rate  = 0.01
+    learning_rate  = 0.011
     reg_param  = 0.02
     for epoch in range(n_epoch):
         for user, movie, value in U_M_train:
-            prediction = mean_rating + user_bias[user] + movie_bias[movie] + np.dot(P[user].T, Q[movie])
+            prediction = mean_rating + user_bias[user] + movie_bias[movie] + np.dot(P[user], Q[movie])
             error = (value - prediction)
             
             user_bias[user] += learning_rate  * (error - reg_param  * user_bias[user])
@@ -50,7 +46,8 @@ def main():
     # вычисление предсказаний и вывод результатов
     for _ in range(T_value):
         user, movie = map(int, input().split())
-        rating = k_value * (mean_rating + user_bias[user] + movie_bias[movie] + np.dot(P[user].T, Q[movie]))
+        # rating = k_value * (mean_rating + user_bias[user] + movie_bias[movie] + np.dot(P[user], Q[movie]))
+        rating = mean_rating + user_bias[user] + movie_bias[movie] + np.dot(P[user], Q[movie])
         rating = min(max(rating, 0), k_value)
         print(rating)
 
