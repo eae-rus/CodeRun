aae34a663cf9b990130b8bb6948fa43b4cb997cb
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-07 00:06:42 +0300

    Изменена "Омографы"
    Обработал более быстрым способом.

--- 2023/ml/Сложные/Омографы/Омографы.py
+++ 2023/ml/Сложные/Омографы/Омографы.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import json
 import re
@@ -29,44 +30,49 @@ def train_input_output_vectors(train_data, fastText_model, train_input_path, tra
     
     for data in train_data:
         index_word += 1
-        if index_word % 1000 == 0:
+        if index_word % 10000 == 0:
             print(index_word)
-            
+        
         # Извлекаем строку из JSON
         source_string = data['source'].split()
-        vectorizer_array = []
-        vectorizer_array_output = [0]*(100* (10+1+10))
+        # предварительная обработка source
         i = 0
-        index_up_word = 0
+        for word in source_string:
+            if has_uppercase(word):
+                index_up_word = i
+                break
+            i += 1
+        temp = []
+        # до
+        if index_up_word < 10:
+            for _ in range(10 - index_up_word):
+                temp.append(".")
+            for sourse_word in source_string[:index_up_word]:
+                temp.append(sourse_word)
+        else:
+            for sourse_word in source_string[index_up_word - 10 :index_up_word]:
+                temp.append(sourse_word)
+        # слово
+        temp.append(source_string[index_up_word])
+        # после
+        if index_up_word == len(source_string)-1:
+            for _ in range(10):
+                temp.append(".")
+        elif (index_up_word+10) - (len(source_string)-1) > 0:
+            for sourse_word in source_string[index_up_word+1:]:
+                temp.append(sourse_word)
+            for _ in range((index_up_word+10) - (len(source_string)-1)):
+                temp.append(".")
+        else:
+            for sourse_word in source_string[index_up_word + 1 : index_up_word + 11]:
+                temp.append(sourse_word)
+        
+        source_string = temp
+        vectorizer_array = []
         for word in source_string:
             vectorizer = fastText_model.wv[word]
             vectorizer_array.append(vectorizer)
-            if has_uppercase(word):
-                index_up_word = i
-            i += 1
-        
-        #
-        if index_up_word < 10:
-            for k in range(index_up_word):
-                array = vectorizer_array[k]
-                vectorizer_array_output[10*100 - index_up_word*100 + k*100 : 10*100 - index_up_word*100 + (k+1)*100] = array
-        else:
-            for k in range(10):
-                array = vectorizer_array[k]
-                vectorizer_array_output[k*100 : (k+1)*100] = array
-        #
-        vectorizer_array_output[10*100:11*100] = vectorizer_array[index_up_word]
-        #
-        if index_up_word+10 > len(source_string)-1:
-            for k in range(index_up_word+1, len(source_string)):
-                array = vectorizer_array[k]
-                vectorizer_array_output[10*100 + (k-index_up_word)*100 : 10*100 + (k+1-index_up_word)*100] = array
-        else:
-            for k in range(index_up_word+1, index_up_word+11):
-                array = vectorizer_array[k]
-                vectorizer_array_output[10*100 + (k-index_up_word)*100 : 10*100 + (k+1-index_up_word)*100] = array
-        
-        train_input.append(vectorizer_array_output)
+        train_input.append(vectorizer_array)
         
         target_string = data['target'].split()
         for word in target_string:
@@ -122,15 +128,28 @@ def main():
     # print(similar_words)    
     
     # Создание и сохранение массива в файл
-    train_input_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_input.pickle'
-    train_output_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_output.pickle'
-    train_input_output_vectors(train_data, fastText_model, train_input_path, train_output_path)
+    # train_input_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_input.pickle'
+    # train_output_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_output.pickle'
+    # train_input_output_vectors(train_data, fastText_model, train_input_path, train_output_path)
     
     # Загрузка массива из файла
-    with open(train_input_path, 'rb') as file:
-        train_input = pickle.load(file)    
-    with open(train_output_path, 'rb') as file:
-        train_output = pickle.load(file)
+    # with open(train_input_path, 'rb') as file:
+    #     train_input = pickle.load(file)    
+    # with open(train_output_path, 'rb') as file:
+    #     train_output = pickle.load(file)
+    
+    train_input_path_np = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_input_np'
+    train_output_path_np = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train_output_np'
+    
+    # train_input_np = np.array(train_input)
+    # train_input_np = train_input_np.reshape(train_input_np.shape[0], -1)
+    # np.save(train_input_path_np, train_input_np)
+    train_input_np = np.load(train_input_path_np)
+    
+    # train_output_np = np.array(train_output)
+    # train_output_np = train_output_np.reshape(train_output_np.shape[0], -1)
+    # np.save(train_output_path_np, train_output_np)
+    train_output_np = np.load(train_output_path_np)
     
     model_SimpleRNN_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\model_SimpleRNN_path.h5'
     # В разработке
