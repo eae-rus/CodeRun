aea83b93584c2c65686281422e0ee25e59c913e4
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-07 19:15:33 +0300

    Изменена "Омографы"
    Продолжаем концепцию, пока без результатно

--- 2023/ml/Сложные/Омографы/Омографы.py
+++ 2023/ml/Сложные/Омографы/Омографы.py
@@ -29,6 +29,10 @@ def extract_vowels(string):
     vowels = re.findall('[аеёиоуыэюя]', string, re.IGNORECASE)
     return ''.join(vowels)
 
+def is_vowel(letter):
+    vowels = re.findall('[аеёиоуыэюя]', letter, re.IGNORECASE)
+    return len(vowels) > 0
+
 def train_input_output_vectors(train_data, fastText_model, train_input_path, train_output_path):
     train_input = []
     train_output = []
@@ -40,46 +44,46 @@ def train_input_output_vectors(train_data, fastText_model, train_input_path, tra
         if index_word % 10000 == 0:
             print(index_word)
         
-        # # Извлекаем строку из JSON
-        # source_string = data['source'].split()
-        # # предварительная обработка source
-        # i = 0
-        # for word in source_string:
-        #     if has_uppercase(word):
-        #         index_up_word = i
-        #         break
-        #     i += 1
-        # temp = []
-        # # до
-        # if index_up_word < 10:
-        #     for _ in range(10 - index_up_word):
-        #         temp.append(".")
-        #     for sourse_word in source_string[:index_up_word]:
-        #         temp.append(sourse_word)
-        # else:
-        #     for sourse_word in source_string[index_up_word - 10 :index_up_word]:
-        #         temp.append(sourse_word)
-        # # слово
-        # temp.append(source_string[index_up_word])
-        # # после
-        # if index_up_word == len(source_string)-1:
-        #     for _ in range(10):
-        #         temp.append(".")
-        # elif (index_up_word+10) - (len(source_string)-1) > 0:
-        #     for sourse_word in source_string[index_up_word+1:]:
-        #         temp.append(sourse_word)
-        #     for _ in range((index_up_word+10) - (len(source_string)-1)):
-        #         temp.append(".")
-        # else:
-        #     for sourse_word in source_string[index_up_word + 1 : index_up_word + 11]:
-        #         temp.append(sourse_word)
-        # 
-        # source_string = temp
-        # vectorizer_array = []
-        # for word in source_string:
-        #     vectorizer = fastText_model.wv[word]
-        #     vectorizer_array.append(vectorizer)
-        # train_input.append(vectorizer_array)
+        # Извлекаем строку из JSON
+        source_string = data['source'].split()
+        # предварительная обработка source
+        i = 0
+        for word in source_string:
+            if has_uppercase(word):
+                index_up_word = i
+                break
+            i += 1
+        temp = []
+        # до
+        if index_up_word < 10:
+            for _ in range(10 - index_up_word):
+                temp.append(".")
+            for sourse_word in source_string[:index_up_word]:
+                temp.append(sourse_word)
+        else:
+            for sourse_word in source_string[index_up_word - 10 :index_up_word]:
+                temp.append(sourse_word)
+        # слово
+        temp.append(source_string[index_up_word])
+        # после
+        if index_up_word == len(source_string)-1:
+            for _ in range(10):
+                temp.append(".")
+        elif (index_up_word+10) - (len(source_string)-1) > 0:
+            for sourse_word in source_string[index_up_word+1:]:
+                temp.append(sourse_word)
+            for _ in range((index_up_word+10) - (len(source_string)-1)):
+                temp.append(".")
+        else:
+            for sourse_word in source_string[index_up_word + 1 : index_up_word + 11]:
+                temp.append(sourse_word)
+        
+        source_string = temp
+        vectorizer_array = []
+        for word in source_string:
+            vectorizer = fastText_model.wv[word]
+            vectorizer_array.append(vectorizer)
+        train_input.append(vectorizer_array)
         
         target_string = data['target'].split()
         vowel_array = [0 for _ in range(6)] # определено, что максимум 6 глассных в исследуюмых текстах
@@ -176,18 +180,18 @@ def main():
         with open(file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=2, ensure_ascii=False, separators=(',', ': '))
 
-    train_data_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train.json'
-    train_data = read_json_file(train_data_path)
+    # train_data_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\train.json'
+    # train_data = read_json_file(train_data_path)
     test_data_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\test.json'
     test_data = read_json_file(test_data_path)
 
 
-    model_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\model_fastText.bin'
+    # model_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\model_fastText.bin'
     # создание и сохранение модели
     # train_and_save_fastText(train_data, model_path)
 
     # Загрузка модели
-    fastText_model = FastText.load(model_path)
+    # fastText_model = FastText.load(model_path)
   
     # тест выдачи похожих слов
     # word_vector = fastText_model.wv['августА']
@@ -232,32 +236,46 @@ def main():
     #------------------------------------
     # обучение модели
     model = tf.keras.Sequential([
-        layers.Dense(1024, activation='relu', input_shape=(2100,)),
+        layers.Dense(2048, activation='relu', input_shape=(2100,)),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(512, activation='relu'),
         layers.Dense(512, activation='relu'),
         layers.Dense(256, activation='relu'),
         layers.Dense(256, activation='relu'),
         layers.Dense(128, activation='relu'),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(64, activation='relu'),
         layers.Dense(64, activation='relu'),
         layers.Dense(32, activation='relu'),
-        layers.Dense(16, activation='relu'),
         layers.Dense(6, activation='softmax')
     ])
     # Компиляция модели
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     # Обучение модели
-    model.fit(train_input_np, train_output_np, epochs=30, batch_size=256)
+    model.fit(train_input_np, train_output_np, epochs=50, batch_size=256)
     
     answer = []
-    for data in test_input_np:
+    for data, test_text in zip(test_input_np, test_data):
         data_test = data.reshape(-1, 1).T
         prediction = model.predict(data_test)
-        topn = 1  # Количество наиболее похожих слов
-        similar_words = fastText_model.wv.most_similar([prediction[0]], topn=topn)
-        answer.append(similar_words[0][0])
-        # print(similar_words[0][0])   
+        word_answer = ''
+        for word in test_text['source'].split():
+            if has_uppercase(word):
+                word_answer = word.lower()
+                break
+        
+        index_vowels = np.argmax(prediction) # max_index 
+        
+        i = 0
+        for i_char in range(len(word_answer)):
+            if is_vowel(word_answer[i_char]):
+                if index_vowels == i:
+                    word_answer = word_answer[:i_char] + word_answer[i_char].upper() + word_answer[i_char+1:]
+                    break
+                else:
+                    i += 1
+        
+        answer.append(word_answer) 
     
     # Сохранение массива в текстовый файл
     answer_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\answer.txt'
@@ -267,8 +285,7 @@ def main():
         for string in answer:
             file.write(string + "\n")
 
-    neyro_model_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\neyro_model'
-    model.save(neyro_model_path)
+    # neyro_model_path = os.path.abspath("") + '\\2023\\ml\\Сложные\\Омографы\\neyro_model'
 
 
 if __name__ == '__main__':
